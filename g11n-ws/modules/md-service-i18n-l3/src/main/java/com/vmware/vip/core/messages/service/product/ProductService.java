fbbdd489a23dd13927f5d56678f35c425b8198fa
Colin Lin <linr@vmware.com>
2019-08-01 15:55:23 +0800

    rm 'fatal error' from log

--- g11n-ws/modules/md-service-i18n-l3/src/main/java/com/vmware/vip/core/messages/service/product/ProductService.java
+++ g11n-ws/modules/md-service-i18n-l3/src/main/java/com/vmware/vip/core/messages/service/product/ProductService.java
@@ -88,8 +88,7 @@ public class ProductService implements IProductService {
 		try {
 			jsonStr = productdao.getVersionInfo(productName, version);
 		} catch (DataException e) {
-			throw new L3APIException(ConstantsKeys.FATA_ERROR
-					+ "Failed to drop version info for " + productName
+			throw new L3APIException("Failed to drop version info for " + productName
 					+ ConstantsChar.BACKSLASH + version, e);
 		}
 		JSONObject jo = JSONUtils.string2JSON(jsonStr);
