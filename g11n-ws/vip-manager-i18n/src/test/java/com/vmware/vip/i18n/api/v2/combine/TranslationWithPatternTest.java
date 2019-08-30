3896eda9e31b57bd106e91c18def4689d883910c
Hu Shi <tigersh123@163.com>
2019-08-30 13:41:03 +0800

    update s3 configuration for Singleton Service s3 build start failed based on https protocol

--- g11n-ws/vip-manager-i18n/src/test/java/com/vmware/vip/i18n/api/v2/combine/TranslationWithPatternTest.java
+++ g11n-ws/vip-manager-i18n/src/test/java/com/vmware/vip/i18n/api/v2/combine/TranslationWithPatternTest.java
@@ -1,7 +1,6 @@
 package com.vmware.vip.i18n.api.v2.combine;
 
 import java.io.File;
-
 import org.apache.commons.io.FileUtils;
 import org.junit.Assert;
 import org.junit.Before;
@@ -13,13 +12,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.web.context.WebApplicationContext;
-
 import com.vmware.vip.BootApplication;
 import com.vmware.vip.common.constants.ConstantsFile;
 import com.vmware.vip.i18n.api.v1.common.CacheUtil;
 import com.vmware.vip.i18n.api.v1.common.ConstantsForTest;
 import com.vmware.vip.i18n.api.v1.common.RequestUtil;
-//import com.vmware.vip.messages.data.bundle.BundleConfig;
+
 
 @RunWith(SpringRunner.class)
 @SpringBootTest(classes = BootApplication.class)
