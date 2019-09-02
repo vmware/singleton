733b62a7153c6140e92619c5751ed0b6c34105b9
Hu Shi <tigersh123@163.com>
2019-09-02 15:53:40 +0800

    please filter out the file from component list

--- g11n-ws/modules/md-data-api-s3impl/src/main/java/com/vmware/vip/messages/data/dao/impl/S3OneComponentDaoImpl.java
+++ g11n-ws/modules/md-data-api-s3impl/src/main/java/com/vmware/vip/messages/data/dao/impl/S3OneComponentDaoImpl.java
@@ -7,13 +7,11 @@ package com.vmware.vip.messages.data.dao.impl;
 import java.io.IOException;
 import java.util.HashMap;
 import java.util.Map;
-
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 import org.springframework.util.StringUtils;
-
 import com.amazonaws.services.s3.model.PutObjectResult;
 import com.amazonaws.services.s3.model.S3Object;
 import com.fasterxml.jackson.core.JsonProcessingException;
