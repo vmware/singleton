1a08f51e70fcfbbd77293a095253784f1ac2bf4e
Xiaochao Li <48587632+Xiaochao8@users.noreply.github.com>
2019-11-01 10:33:39 +0800

    Update ConstantsMsg.java

--- src/main/java/com/vmware/vipclient/i18n/common/ConstantsMsg.java
+++ src/main/java/com/vmware/vipclient/i18n/common/ConstantsMsg.java
@@ -12,7 +12,6 @@ public class ConstantsMsg {
 	public static final String SERVER_RETURN_EMPTY = "Server returned empty.";
 	public static final String SERVER_RETURN_ERROR = "Server returned error! Status: %d. Message: %s";
 	public static final String SERVER_CONTENT_ERROR = "The content from server is wrong!";
-
 	public static final String UNKNOWN_ERROR = "Unknown error.";
 
 }
