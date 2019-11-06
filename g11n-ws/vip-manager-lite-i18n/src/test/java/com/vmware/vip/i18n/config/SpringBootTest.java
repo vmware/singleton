92fccd8d9da8e822a0f92ecdc7b84b74528f704c
Hu Shi <tigersh123@163.com>
2019-11-06 16:33:41 +0800

    merge saas and lite version

---
+++ g11n-ws/vip-manager-lite-i18n/src/test/java/com/vmware/vip/i18n/config/SpringBootTest.java
@@ -0,0 +1,27 @@
+package com.vmware.vip.i18n.config;
+
+import org.junit.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import com.vmware.vip.BootApplication;
+
+public class SpringBootTest {
+	private static Logger logger = LoggerFactory.getLogger(SpringBootTest.class);
+
+	@Test
+	public void test001serviceswich() {
+		String[] args = { "-c" };
+		try {
+			BootApplication.main(args);
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			logger.error(e.getMessage(), e);
+		}
+		
+
+		logger.info("TestSpringBoot");
+	}
+
+
+}
