5c4db82e4adf0792bfa7148cb16a6a0931adb98e
Hu Shi <tigersh123@163.com>
2019-11-07 17:55:02 +0800

    cover unit test

--- g11n-ws/vip-manager-l10n/src/test/java/com/vmware/test/TestModels.java
+++ g11n-ws/vip-manager-l10n/src/test/java/com/vmware/test/TestModels.java
@@ -3,12 +3,21 @@ package com.vmware.test;
 
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.Map;
 
 import org.junit.Test;
+import org.springframework.beans.BeanUtils;
 
 import com.vmware.l10n.record.model.ComponentSourceModel;
 import com.vmware.l10n.source.dto.SourceDTO;
+import com.vmware.l10n.translation.dto.ComponentMessagesDTO;
 import com.vmware.l10n.translation.dto.UpdateListDTO;
+import com.vmware.l10n.utils.SourceCacheUtils;
+import com.vmware.vip.common.constants.ConstantsKeys;
+import com.vmware.vip.common.exceptions.VIPCacheException;
+import com.vmware.vip.common.l10n.source.dto.ComponentSourceDTO;
+import com.vmware.vip.common.l10n.source.dto.StringSourceDTO;
+import com.vmware.vip.common.l10n.source.util.PathUtil;
 import  com.vmware.l10n.source.dto.GRMResponseDTO;
 
 public class TestModels {
@@ -59,5 +68,33 @@ public class TestModels {
         dto.getName();
         dto.getSubList();
         
+    }
+    
+    @Test 
+    public void testSourceCacheUtils() {
+        StringSourceDTO sourceDTO = new StringSourceDTO();
+        sourceDTO.setProductName("devCenter");
+        sourceDTO.setVersion("1.0.0");
+        sourceDTO.setComponent("default");
+        sourceDTO.setLocale(ConstantsKeys.LATEST);
+        sourceDTO.setKey("dc.myhome.open3");
+        sourceDTO.setSource("this open3's value");
+        sourceDTO.setComment("dc new string");
+        ComponentSourceDTO comp = new ComponentSourceDTO();
+        BeanUtils.copyProperties(sourceDTO, comp);
+        comp.setMessages(sourceDTO.getKey(), sourceDTO.getSource());
+        String catcheKey = PathUtil.generateCacheKey(sourceDTO);
+        try {
+            SourceCacheUtils.addSourceCache(catcheKey, comp);
+            SourceCacheUtils.getSourceCache(catcheKey);
+            SourceCacheUtils.updateSourceCache(catcheKey, comp);
+            SourceCacheUtils.delSourceCacheByKey(catcheKey);
+            SourceCacheUtils.addSourceCache(catcheKey, comp);
+            SourceCacheUtils.getSourceCacheWithDel(catcheKey);
+            
+        } catch (VIPCacheException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
     }
 }
