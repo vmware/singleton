7b02e4590cb2355d5a7dc8e26d50ad15cff22d80
tigershi <shihu@vmware.com>
2019-11-08 14:34:47 +0800

    merge saas and lite version (#269)
    merge saas and lite version

---
+++ g11n-ws/vip-manager-lite-i18n/src/test/java/com/vmware/vip/i18n/api/v1/date/DateAPITest.java
@@ -0,0 +1,38 @@
+package com.vmware.vip.i18n.api.v1.date;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.web.context.WebApplicationContext;
+
+import com.vmware.vip.LiteBootApplication;
+import com.vmware.vip.i18n.api.v1.common.CacheUtil;
+import com.vmware.vip.i18n.api.v1.common.ConstantsForTest;
+import com.vmware.vip.i18n.api.v1.common.RequestUtil;
+
+/**
+ * Test cases for DateController.
+ *
+ */
+@RunWith(SpringRunner.class)
+@SpringBootTest(classes = LiteBootApplication.class)
+public class DateAPITest {
+
+    @Autowired
+    private WebApplicationContext webApplicationContext;
+    
+    @Before
+    public void authentication() throws Exception{
+        String authenticationResult=RequestUtil.sendRequest(webApplicationContext,ConstantsForTest.POST, ConstantsForTest.AuthenticationAPIURI);
+        CacheUtil.cacheSessionAndToken(webApplicationContext, authenticationResult);
+    }
+
+    @Test
+    public void testLocalizedDateAPI() throws Exception {
+        RequestUtil.sendRequest(webApplicationContext,ConstantsForTest.GET, ConstantsForTest.DateAPIURI);
+    }
+
+}
---
+++ g11n-ws/vip-manager-lite-i18n/src/test/java/com/vmware/vip/i18n/config/ConfigurationTest.java
@@ -0,0 +1,58 @@
+package com.vmware.vip.i18n.config;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.web.context.WebApplicationContext;
+
+import com.vmware.vip.LiteBootApplication;
+import com.vmware.vip.core.conf.ServerProperties;
+import com.vmware.vip.i18n.api.v1.common.CacheUtil;
+import com.vmware.vip.i18n.api.v1.common.ConstantsForTest;
+import com.vmware.vip.i18n.api.v1.common.RequestUtil;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(classes = LiteBootApplication.class)
+public class ConfigurationTest {
+
+	@Autowired
+	private WebApplicationContext webApplicationContext;
+
+	
+	@Test
+	public void testServerProperites() {
+	    ServerProperties sp = webApplicationContext.getBean(ServerProperties.class);
+	    sp.getServerPort();
+	    sp.getServerScheme();
+	    sp.getHttpPort();
+	    sp.getHttpsKeyStore();
+	    sp.getHttpsKeyStorePassword();
+	    sp.getHttpsKeyAlias();
+	    sp.isAllowTrace();
+	}
+	
+	
+	@Test
+    public void testServerProperites1() {
+        ServerProperties sp = new ServerProperties();
+        sp.setServerPort(300);
+        sp.setServerScheme("testScheme");
+        sp.setHttpPort(123);
+        sp.getServerPort();
+        sp.getServerScheme();
+        sp.getHttpPort();
+        sp.setHttpsKeyStore("testkeyStore");
+        sp.getHttpsKeyStore();
+        sp.setHttpsKeyStorePassword("testpassword");
+        sp.getHttpsKeyStorePassword();
+        sp.setHttpsKeyAlias("testAlias");
+        sp.getHttpsKeyAlias();
+        sp.setAllowTrace(true);
+        sp.isAllowTrace();
+    }
+
+
+}
