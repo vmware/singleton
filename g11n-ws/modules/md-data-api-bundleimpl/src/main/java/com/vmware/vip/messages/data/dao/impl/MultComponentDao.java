49fc83d32f51cd839c147aa3ea397536a5604a13
tigershi <shihu@vmware.com>
2019-11-12 09:58:43 +0800

    fixed the mult-component get big size translation error (#277)
    * fixed the mult-component get big size translation error

--- g11n-ws/modules/md-data-api-bundleimpl/src/main/java/com/vmware/vip/messages/data/dao/impl/MultComponentDao.java
+++ g11n-ws/modules/md-data-api-bundleimpl/src/main/java/com/vmware/vip/messages/data/dao/impl/MultComponentDao.java
@@ -42,10 +42,18 @@ public class MultComponentDao implements IMultComponentDao {
 		}
 		for (String component : components) {
 			for (String locale : locales) {
+			    try {
 				bundles.add(oneComponentDao.get2JsonStr(productName, version,
 						component, locale));
+			} catch (DataException e) {
+               logger.error(e.getMessage(), e);
+            }
 			}
 		}
+		  if (bundles.isEmpty()) {
+	            throw new BundleException("Failed to get for "
+                        + productName + ConstantsChar.BACKSLASH + version);
+	        }
 		logger.debug("end get2JsonStrs");
 		return bundles;
 	}
@@ -65,9 +73,7 @@ public class MultComponentDao implements IMultComponentDao {
 					bundles.add(oneComponentDao.get(productName, version,
 							component, locale));
 				} catch (DataException e) {
-					throw new BundleException("Failed to get for "
-							+ productName + ConstantsChar.BACKSLASH + version + ConstantsChar.BACKSLASH + component
-							+ ConstantsChar.BACKSLASH + locale + ".", e);
+				    logger.error(e.getMessage(), e);
 				}
 			}
 		}
@@ -75,7 +81,7 @@ public class MultComponentDao implements IMultComponentDao {
 		logger.debug("end get");
 
 		if (bundles.isEmpty()) {
-			throw new BundleException("No bundle is found.");
+		    throw new BundleException("Failed to get for " + productName + ConstantsChar.BACKSLASH + version);
 		}
 		return bundles;
 	}
