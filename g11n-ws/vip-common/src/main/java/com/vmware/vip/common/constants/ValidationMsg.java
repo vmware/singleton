e00c97e4d60ab6136e5786013f8c361d5bc54110
Tiger Shi <tigersh123@163.com>
2020-03-26 12:42:12 +0800

    add the new validate feature

--- g11n-ws/vip-common/src/main/java/com/vmware/vip/common/constants/ValidationMsg.java
+++ g11n-ws/vip-common/src/main/java/com/vmware/vip/common/constants/ValidationMsg.java
@@ -8,8 +8,14 @@ public final class ValidationMsg {
 	public final static String PRODUCTNAME_NOT_VALIDE = "Incorrect produtName(only allows letter and number)";
 	public final static String VERSION_NOT_VALIDE = "Incorrect version(only allows number and dot, e.g. 1.2.0)";
 	public final static String COMPONENT_NOT_VALIDE = "Incorrect component(only allows letter, number, dot, underline, dashline)";
+	public final static String COMPONENTS_NOT_VALIDE = "Incorrect components(only allows letter, number, comma, dot, underline, dashline)";
 	public final static String KEY_NOT_VALIDE = "Incorrect key(only allows letter, dot, underline, dashline)";
 	public final static String LOCALE_NOT_VALIDE = "Incorrect locale(only allows letter, number, dot, underline, dashline)";
+	public final static String LOCALES_NOT_VALIDE = "Incorrect locales(only allows letter, number, comma, dot, underline, dashline)";
+	public final static String LANGUAGE_NOT_VALIDE = "Incorrect language(only allows letter, number, dot, underline, dashline)";
+	public final static String REGION_NOT_VALIDE = "Incorrect region(only allows letter, number, dot, underline, dashline)";
+	public final static String COMBINE_NOT_VALIDE = "Incorrect combine(only 1 or 2)";
+	public final static String SCOPE_NOT_VALIDE = "Incorrect scope(allows letter, comma)";
 	public final static String COLLECTSOURCE_NOT_VALIDE = "Incorrect collectsource(only allows true, false)";
 	public final static String PSEUDO_NOT_VALIDE = "Incorrect pseudo(only allows true, false)";
 	public final static String NUMBER_NOT_VALIDE = "Incorrect number";
