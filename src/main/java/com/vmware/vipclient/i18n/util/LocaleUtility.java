5cd06d60a04a85e57a1fe4f5aeb9d8537d2e2ed7
Jessie <jessiejuachon@gmail.com>
2020-05-14 11:53:40 -0700

    code clean up

--- src/main/java/com/vmware/vipclient/i18n/util/LocaleUtility.java
+++ src/main/java/com/vmware/vipclient/i18n/util/LocaleUtility.java
@@ -14,11 +14,11 @@ import java.util.stream.Collectors;
 
 public class LocaleUtility {
 
-    private static Locale defaultLocale = Locale.ENGLISH;
-    private static Locale sourceLocale;
+    private static Locale defaultLocale;
+    private static Locale sourceLocale = Locale.ENGLISH;
     
     // A locale fallback priority queue. For now, it only contains the default locale and the source messages (from "messages_source.json").
-    private static List<Locale> fallbackLocales = new LinkedList<Locale>(Arrays.asList(defaultLocale, Locale.forLanguageTag(ConstantsKeys.SOURCE)));
+    private static List<Locale> fallbackLocales = new LinkedList<Locale>(Arrays.asList(getDefaultLocale(), Locale.forLanguageTag(ConstantsKeys.SOURCE)));
     
     // Use ThreadLocal to combine the locale with local thread so that the
     // locale can be used by any code places.
@@ -224,7 +224,7 @@ public class LocaleUtility {
     }
 
 	public static Locale getDefaultLocale() {
-		return defaultLocale;
+		return defaultLocale == null ? getSourceLocale() : defaultLocale;
 	}
 
 	public static void setDefaultLocale(Locale defaultLocale) {
@@ -232,7 +232,7 @@ public class LocaleUtility {
 	}
 
 	public static Locale getSourceLocale() {
-		return sourceLocale == null ? getDefaultLocale() : sourceLocale; 
+		return sourceLocale; 
 	}
 
 	public static void setSourceLocale(Locale sourceLocale) {
