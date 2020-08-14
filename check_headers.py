6c0839b422b48e589f1f93bf8ccce80d75f79485
surite <surite@live.cn>
2020-08-14 15:36:23 +0800

    show header pattern in logs

--- check_headers.py
+++ check_headers.py
@@ -39,7 +39,7 @@ import datetime
 SOURCE_CODE_EXTENSIONS = [".java", ".go", ".ts", ".js", ".asp", ".aspx", ".jsp", ".html", ".css", ".php",
 ".sh", ".py", ".rb", ".cpp", ".c", ".cs", ".h", ".hpp", ".swift", ".sql", ".vb", ".ps1", ".m", ".mm", ".gradle", ".bat", ".xml"]
 REQUIRED_PATTERN_LIST = ["makefile", "dockerfile"] #name list of file must to be checked even extension name is not in the list or has no extension name, case not sensitive
-SINGLETON_PATTERN = ".*Copyright ([1-9][0-9]{3}-)?{} VMware, Inc\.\s+\n.*SPDX-License-Identifier: EPL-2.0\s+$".format(datetime.datetime.now().year)
+SINGLETON_PATTERN = ".*Copyright ([1-9][0-9]{3}-)?"+datetime.datetime.now().year+" VMware, Inc\.\s+\n.*SPDX-License-Identifier: EPL-2.0\s+$"
 NOT_REQUIRED_PATTERN_LIST = ["__init__.py", ".*.designer.cs"]
 PATH_SEPARATOR = ":"
 EXTENSION_SEPARATOR = ":"
@@ -150,6 +150,7 @@ if __name__ == "__main__":
         print("Files miss license or copyright headers:\n    {}".format("\n    ".join(errs)))
         print("========================================= Fail =========================================\n" +
               "    Missing headers in {} files, see file list above.\n".format(len(errs)) +
+              "    Header pattern '{}'".format(headers_pattern) +
               "    Refer to contributing guide to see how to add license and copyright headers.\n" +
               "========================================================================================")
         sys.exit(1)
