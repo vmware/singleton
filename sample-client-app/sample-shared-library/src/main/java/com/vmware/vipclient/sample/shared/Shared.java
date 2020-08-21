57aeb9b1403613865911e1adea5789dad1b0d338
Jessie <jessiejuachon@gmail.com>
2020-08-21 07:23:49 -0700

    sample shared library

---
+++ sample-client-app/sample-shared-library/src/main/java/com/vmware/vipclient/sample/shared/Shared.java
@@ -0,0 +1,21 @@
+package com.vmware.vipclient.sample.shared;
+
+import java.util.Locale;
+import com.vmware.vipclient.i18n.base.instances.TranslationMessage;
+
+public class Shared {
+    TranslationMessage tm = null;
+    Locale locale = null;
+    static String component = "shared";
+
+    public Shared(TranslationMessage tm , Locale locale) {
+        this.tm = tm;
+        this.locale = locale;
+    }
+
+    public String someMethod () {
+        System.out.println(">>>>>> Shared.someMethod");
+        String msg = tm.getMessage(locale, component, "shared.library.key1");
+        return msg;
+    }
+}
