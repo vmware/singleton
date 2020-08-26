e043197ae11891f30df588193618f7fb5fa8c579
jessiejuachon <jessiejuachon@gmail.com>
2020-08-26 09:50:32 -0700

    G11n java client - Sample application with shared library (#735)
    Sample application that uses a shared library that uses VIP java client library

---
+++ sample-client-app/sample-app-with-shared-lib/src/main/java/com/vmware/vipclient/sample/Main.java
@@ -0,0 +1,46 @@
+/*
+ * Copyright 2019-2020 VMware, Inc.
+ * SPDX-License-Identifier: EPL-2.0
+ */
+
+package com.vmware.vipclient.sample;
+
+import java.util.Locale;
+
+import com.vmware.vipclient.i18n.I18nFactory;
+import com.vmware.vipclient.i18n.VIPCfg;
+import com.vmware.vipclient.i18n.base.cache.FormattingCache;
+import com.vmware.vipclient.i18n.base.cache.MessageCache;
+import com.vmware.vipclient.i18n.base.instances.TranslationMessage;
+import com.vmware.vipclient.i18n.exceptions.VIPClientInitException;
+import com.vmware.vipclient.i18n.util.LocaleUtility;
+
+public class Main {
+	
+	public static void main(String[] args) {
+		Locale sharedLibLocale = Locale.forLanguageTag("es");
+		if (args.length > 0) {
+			// set locale
+			System.out.println("Set locale to: " + args[0]);
+			sharedLibLocale = Locale.forLanguageTag(args[0]);
+		}
+
+		// Initialize root application's VIP configuration
+		VIPCfg cfg = VIPCfg.getInstance();
+		try {
+			cfg.initialize("sampleconfig");
+		} catch (VIPClientInitException e) {
+			System.out.println(e.getMessage());
+		}
+		cfg.initializeVIPService();
+		cfg.createTranslationCache(MessageCache.class);
+		cfg.createFormattingCache(FormattingCache.class);
+
+		TranslationMessage tm = (TranslationMessage) I18nFactory.getInstance(cfg).getMessageInstance(TranslationMessage.class);
+		String rootMsg = tm.getMessage(Locale.forLanguageTag("fr"), "default", "global_text_username");
+		System.out.println("Root application's message; fetch is triggered from the root application:  " + rootMsg);
+
+		SharedLibDemo.demoSharedLib(sharedLibLocale);
+
+	}
+}
---
+++ sample-client-app/sample-shared-library/README.md
@@ -0,0 +1,29 @@
+Singleton Java Client Sample Shared Library
+============
+
+This library can be used to demonstrate usage of the Singleton Java client in a shared library.
+
+Prerequisites
+------------
+ * Build the Singleton Java client library by following the instructions in [here](https://github.com/vmware/singleton/blob/g11n-java-client/README.md).
+
+How to build and run this sample shared library
+------------
+ * Go to the root directory of this sample application
+   ```
+   g11n-java-client/sample/-client-app/sample-shared-library
+   ```
+ * Create a lib folder.
+   ```
+   mkdir lib
+   ```
+ * Put the Singleton Java client library into the lib folder.
+   ```
+   cp ../../build/libs/vip4java-0.1.0.jar ./lib/
+   ```
+ * Build the sample application.
+   ```
+   gradle build
+   ```
+Note: The library jar will be created under "build/libs" directory. This can be used as a shared library in one or more applications. See sample in [here](https://github.com/vmware/singleton/blob/g11n-java-client/sample-client-app/sample-app-with-shared-lib/README.md)
+
