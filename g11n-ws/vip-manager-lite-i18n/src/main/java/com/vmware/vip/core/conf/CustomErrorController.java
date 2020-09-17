0f12693997c67b7f426ed6e47cba12c5b63ae961
Colin Lin <linr@vmware.com>
2020-09-17 11:02:29 +0800

    add copyright info

--- g11n-ws/vip-manager-lite-i18n/src/main/java/com/vmware/vip/core/conf/CustomErrorController.java
+++ g11n-ws/vip-manager-lite-i18n/src/main/java/com/vmware/vip/core/conf/CustomErrorController.java
@@ -1,3 +1,7 @@
+/*
+ * Copyright 2019-2020 VMware, Inc.
+ * SPDX-License-Identifier: EPL-2.0
+ */
 package com.vmware.vip.core.conf;
 import org.springframework.boot.web.servlet.error.ErrorController;
 import org.springframework.web.bind.annotation.RequestMapping;
