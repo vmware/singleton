d884ed55f65fe63b0b7571dc106064df7a577b45
Jessie <jessiejuachon@gmail.com>
2020-11-04 20:27:27 -0800

    Adding LocalSrcMessagesPropsOpt.java

---
+++ src/main/java/com/vmware/vipclient/i18n/messages/api/opt/local/LocalSrcMessagesPropsOpt.java
@@ -0,0 +1,104 @@
+/*
+ * Copyright 2019-2020 VMware, Inc.
+ * SPDX-License-Identifier: EPL-2.0
+ */
+package com.vmware.vipclient.i18n.messages.api.opt.local;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.nio.file.*;
+import java.util.*;
+import java.util.stream.Stream;
+
+import com.vmware.vipclient.i18n.VIPCfg;
+import com.vmware.vipclient.i18n.base.DataSourceEnum;
+import com.vmware.vipclient.i18n.base.cache.MessageCacheItem;
+import com.vmware.vipclient.i18n.messages.api.opt.MessageOpt;
+import com.vmware.vipclient.i18n.messages.api.opt.Opt;
+import com.vmware.vipclient.i18n.messages.dto.MessagesDTO;
+import com.vmware.vipclient.i18n.messages.service.ProductService;
+import com.vmware.vipclient.i18n.util.ConstantsKeys;
+import com.vmware.vipclient.i18n.util.FormatUtils;
+import com.vmware.vipclient.i18n.util.JSONBundleUtil;
+import org.json.simple.JSONObject;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class LocalSrcMessagesPropsOpt implements Opt, MessageOpt {
+
+	private Logger logger = LoggerFactory.getLogger(LocalSrcMessagesPropsOpt.class.getName());
+
+	private static final String PROPERTIES_SUFFIX = ".properties";
+    private MessagesDTO dto;
+
+    public LocalSrcMessagesPropsOpt(MessagesDTO dto) {
+        this.dto = dto;
+    }
+
+    @Override
+    public void getComponentMessages(MessageCacheItem cacheItem) {
+		Map<String, String> messages = new HashMap<>();
+
+		if (dto.getLocale().equals(ConstantsKeys.SOURCE)) {
+			try {
+				Path path = Paths.get(VIPCfg.getInstance().getOfflineResourcesBaseUrl(), dto.getComponent());
+				URI uri = Thread.currentThread().getContextClassLoader().
+						getResource(path.toString()).toURI();
+				if (uri.getScheme().equals("jar")) {
+					try(FileSystem fileSystem = FileSystems.newFileSystem(uri, Collections.<String, Object>emptyMap())) {
+						path = fileSystem.getPath(path.toString());
+						loadProps(messages, path);
+					}
+				} else {
+					path = Paths.get(uri);
+					loadProps(messages, path);
+				}
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+		}
+
+		if (!messages.isEmpty())
+			cacheItem.setCacheItem(messages, null, System.currentTimeMillis(), null);
+
+	}
+
+	private void loadProps(Map<String, String> messages, Path offlineResourcePath) throws IOException {
+		try (Stream<Path> listOfFiles = Files.walk(offlineResourcePath).filter(p ->
+				Files.isRegularFile(p) && p.getFileName().toString().endsWith(PROPERTIES_SUFFIX))) {
+			listOfFiles.map(propsFile -> {
+				try {
+					return readProps(propsFile);
+				} catch (IOException e) {
+					return new Properties();
+				}
+			}).forEach(properties -> {
+				Set<String> keys = properties.stringPropertyNames();
+				for (String key : keys) {
+					messages.put(key, properties.getProperty(key));
+				}
+			});
+		}
+	}
+
+	private Properties readProps(Path propsFile) throws IOException {
+		try (InputStream stream = Files.newInputStream(propsFile)) {
+			Properties config = new Properties();
+			config.load(stream);
+			return config;
+		}
+	}
+    
+    private List<Locale> getSupportedLocales() {
+		ProductService ps = new ProductService(dto);
+		Set<String> supportedLanguages = ps.getSupportedLanguageTags(DataSourceEnum.Bundle);
+        logger.debug("supported languages: [{}]", supportedLanguages.toString());
+    	List<Locale> supportedLocales = new LinkedList<Locale>();
+    	for (String languageTag : supportedLanguages) {
+    		supportedLocales.add(Locale.forLanguageTag(languageTag));
+    	}
+    	return supportedLocales;
+    }
+}
