b55e685870091baa74bf354458e566ce504e1038
Jessie <jessiejuachon@gmail.com>
2020-12-05 11:30:48 -0800

    closing input stream

--- src/main/java/com/vmware/vipclient/i18n/messages/api/opt/local/LocalMessagesOpt.java
+++ src/main/java/com/vmware/vipclient/i18n/messages/api/opt/local/LocalMessagesOpt.java
@@ -18,6 +18,7 @@ import org.json.simple.parser.JSONParser;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.net.URL;
@@ -43,16 +44,23 @@ public class LocalMessagesOpt implements Opt, MessageOpt {
     
     @Override
     public void getComponentMessages(MessageCacheItem cacheItem) {
+		InputStream is = getInputStream();
 		try {
-			InputStream is = getInputStream();
 			JSONParser jsonParser = new JSONParser();
 			JSONObject jsonObject = (JSONObject) jsonParser.parse(new InputStreamReader(is, "UTF-8"));
 			Map<String, String> messages = (JSONObject) jsonObject.get("messages");
 			cacheItem.setCacheItem(messages, null, System.currentTimeMillis(), null);
 		} catch (Exception e) {
-			logger.debug(e.getMessage());
+			logger.error("Failed to get offline messages for product: " + dto.getProductID() + " " + dto.getVersion() +
+					", component: " + dto.getComponent() + ", locale: " + dto.getLocale() + ", exception: " + e.getMessage());
+		} finally {
+			try {
+				is.close();
+			} catch (IOException e) {
+				logger.debug(e.getMessage());
+			}
 		}
-    }
+	}
 
 	private InputStream getInputStream() {
 		String locale = LocaleUtility.fmtToMappedLocale(dto.getLocale()).toLanguageTag();
