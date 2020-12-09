896f2bd49904361d2ee94b399362a41e9faf39f2
Xiaochao Li <48587632+Xiaochao8@users.noreply.github.com>
2020-12-09 15:05:30 +0800

    Add L10n s3 access key encryption (#901)

--- g11n-ws/vip-manager-l10n/src/main/java/com/vmware/l10n/conf/S3Client.java
+++ g11n-ws/vip-manager-l10n/src/main/java/com/vmware/l10n/conf/S3Client.java
@@ -8,7 +8,7 @@ import javax.annotation.PostConstruct;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Value;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 
@@ -26,91 +26,30 @@ import com.amazonaws.services.s3.model.GetBucketLocationRequest;
 public class S3Client {
 	private static Logger logger = LoggerFactory.getLogger(S3Client.class);
 
-	/**
-	 * the s3 password is encryption or not
-	 */
-	@Value("${s3.keysEncryptEnable:false}")
-	private boolean encryption;
+	@Autowired
+	private S3Cfg config;
 
-	/**
-	 * the s3 password public key used to decrypt data
-	 */
-	@Value("${s3.publicKey}")
-	private String publicKey;
-
-	/**
-	 * the s3 access Key
-	 */
-	@Value("${s3.accessKey}")
-	private String accessKey;
-
-	/**
-	 * the s3 secret key
-	 */
-	@Value("${s3.secretkey}")
-	private String secretkey;
-
-	/**
-	 * the s3 region name
-	 */
-	@Value("${s3.region}")
-	private String s3Region;
-
-	/**
-	 * the s3 bucket Name
-	 */
-	@Value("${s3.bucketName}")
-	private String bucketName;
-
-	private AmazonS3 s3Inst;
+	private AmazonS3 s3Client;
 
 	/**
 	 * initialize the the S3 client environment
 	 */
 	@PostConstruct
 	private void init() {
-		s3Inst = AmazonS3ClientBuilder.standard()
+		s3Client = AmazonS3ClientBuilder.standard()
 				.withCredentials(new AWSStaticCredentialsProvider(
-						new BasicAWSCredentials(this.getAccessKey(), this.getSecretkey())))
-				.withRegion(s3Region).enablePathStyleAccess().build();
-		if (!s3Inst.doesBucketExistV2(bucketName)) {
-			s3Inst.createBucket(bucketName);
+						new BasicAWSCredentials(config.getAccessKey(), config.getSecretkey())))
+				.withRegion(config.getS3Region()).enablePathStyleAccess().build();
+		if (!s3Client.doesBucketExistV2(config.getBucketName())) {
+			s3Client.createBucket(config.getBucketName());
 			// Verify that the bucket was created by retrieving it and checking its
-			// location.
-			String bucketLocation = s3Inst.getBucketLocation(new GetBucketLocationRequest(bucketName));
+			String bucketLocation =
+					s3Client.getBucketLocation(new GetBucketLocationRequest(config.getBucketName()));
 			logger.info("Bucket location: {}", bucketLocation);
 		}
 	}
 
 	public AmazonS3 getS3Client() {
-		return s3Inst;
-	}
-	
-	
-
-	public String getAccessKey() {
-		if (this.encryption) {
-			try {
-				return RsaCryptUtil.decryptData(this.getAccessKey(), this.publicKey);
-			} catch (Exception e) {
-				logger.error(e.getMessage(), e);
-				return null;
-			}
-		} else {
-			return this.accessKey;
-		}
-	}
-
-	public String getSecretkey() {
-		if (this.encryption) {
-			try {
-				return RsaCryptUtil.decryptData(this.secretkey, this.publicKey);
-			} catch (Exception e) {
-				logger.error(e.getMessage(), e);
-				return null;
-			}
-		} else {
-			return this.secretkey;
-		}
+		return s3Client;
 	}
 }
