eca309182f84d6e75664885946b89cc85b5aa1ce
xiaochao8 <48587632+Xiaochao8@users.noreply.github.com>
2020-12-15 07:30:12 -0800

    45555555555555

--- g11n-ws/vip-manager-l10n/src/main/java/com/vmware/l10n/conf/S3Client.java
+++ g11n-ws/vip-manager-l10n/src/main/java/com/vmware/l10n/conf/S3Client.java
@@ -4,20 +4,18 @@
  */
 package com.vmware.l10n.conf;
 
-import javax.annotation.PostConstruct;
-
-import com.vmware.vip.common.constants.ConstantsChar;
+import com.amazonaws.auth.AWSStaticCredentialsProvider;
+import com.amazonaws.auth.BasicAWSCredentials;
+import com.amazonaws.services.s3.AmazonS3;
+import com.amazonaws.services.s3.AmazonS3ClientBuilder;
+import com.amazonaws.services.s3.model.GetBucketLocationRequest;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 
-import com.amazonaws.auth.AWSStaticCredentialsProvider;
-import com.amazonaws.auth.BasicAWSCredentials;
-import com.amazonaws.services.s3.AmazonS3;
-import com.amazonaws.services.s3.AmazonS3ClientBuilder;
-import com.amazonaws.services.s3.model.GetBucketLocationRequest;
+import javax.annotation.PostConstruct;
 
 /**
  * the configuration of the S3 client
