25eeb8b8237fcfae28b71a22c17e6ac4ad639940
lyiyu66 <lyiyu66@hotmail.com>
2020-12-16 12:00:59 +0800

    Update test case as cldr data is stored with zip format

--- autotest/cldrpattern/test.py
+++ autotest/cldrpattern/test.py
@@ -2,11 +2,40 @@ import os,json,HTMLTestRunner,unittest
 from excelutility import excelutil
 from ddt import ddt,data
 from datetime import datetime
+from zipfile import ZipFile
 import constant
 
 
 def curfoldercount(dir):
     return str(len(os.listdir(dir)))
+
+def curfilecount(dir):
+    return str(len(os.listdir(dir)))
+
+
+def readzipfile(zipdir, innerzipdir, filename, key ):
+    with ZipFile(constant.rootdir+zipdir) as zf:
+        file_data = zf.read(innerzipdir+filename)
+        json_str = file_data.decode('utf-8')
+        data = eval(json_str)
+        keylist = key.split('.')
+        for i in keylist:
+            data = data.get(i)
+    return str(data)
+
+
+# result = readzipfile('./singleton-g11n-cldr-pattern/src/main/resources/cldr/data/32.0.0/cldr-core-32.0.0.zip', 'cldr-core-32.0.0/supplemental' ,'/aliases.json', 'supplemental.metadata.alias.languageAlias.zyb._reason')
+# print('result is: %s' % result)
+
+
+
+def readjson(dir, filename, key):
+    with open(dir+filename, 'r', encoding='utf-8') as f:
+        temp = json.load(f)
+        keylist = key.split('.')
+        for i in keylist:
+            temp = temp[i]
+    return str(temp)
 
 
 def rootfoldercount(dir):
@@ -28,15 +57,18 @@ def curfilename(dir):
         L.extend(files)
     return L
 
+def unzipfile(dir, path):
+    try:
+        filename = curfilename(dir)
+        for i in filename:
+            with ZipFile(constant.rootdir + constant.version + "/" + i) as zf:
+                for f in zf.namelist():
+                    zf.extract(f, path)
+        zf.close()
+    except:
+        print(f'{i} is unzip failed.')
 
-
-def readjson(dir, filename, key):
-    with open(dir+filename, 'r', encoding='utf-8') as f:
-        temp = json.load(f)
-        keylist = key.split('.')
-        for i in keylist:
-            temp = temp[i]
-    return str(temp)
+# unzipfile('./singleton-g11n-cldr-pattern/src/main/resources/cldr/data/32.0.0', constant.unzipfile_dir)
 
 
 
@@ -46,17 +78,27 @@ class Test(unittest.TestCase):
     @data(*excelutil())
     def test(self, case):
 
+
+
+        #Check the count of languages
         if (case['category'] == 'FolderCountCheck'):
             print('############Case ID: %s #####Case Name: %s############' % (case['caseid'], case['casename']))
-            curfolder = curfoldercount(constant.rootdir + case['path'])
+            curfolder = curfoldercount(constant.unzipfile_dir + case['innerzipdir'])
             self.assertEqual(curfolder, case['expected'])
+        #Check the count of files
+        if (case['category'] == 'FileCountCheck'):
+            print('############Case ID: %s #####Case Name: %s############' % (case['caseid'], case['casename']))
+            curfile = curfilecount(constant.unzipfile_dir + case['innerzipdir'])
+            self.assertEqual(curfile, case['expected'])
+        #Check the included files are correct and no loss
         elif (case['category'] == 'FileNameCheck'):
             print('############Case ID: %s #####Case Name: %s############' % (case['caseid'], case['casename']))
-            curfileList = curfilename(constant.rootdir + case['path'])
+            curfileList = curfilename(constant.rootdir + case['zipdir'])
             self.assertEqual(curfileList.sort(), eval(case['expected']).sort())
+        #Check the data in json file is correct
         elif (case['category'] == 'JsonValueCheck'):
             print('############Case ID: %s #####Case Name: %s############' % (case['caseid'], case['casename']))
-            result = readjson(constant.rootdir + case['path'], case['filename'], case['key'])
+            result = readzipfile(case['zipdir'], case['innerzipdir'], case['filename'], case['key'])
             self.assertEqual(result, case['expected'])
 
 
@@ -64,6 +106,7 @@ class Test(unittest.TestCase):
 if __name__ == "__main__":
 
     print('=====AutoTest Start======')
+    unzipfile(constant.rootdir + constant.version, constant.unzipfile_dir)
     discover = unittest.defaultTestLoader.discover(constant.test_dir, pattern='test.py')
     now = datetime.now().strftime('%Y-%m-%d_%H_%M_%S_')
     filename = constant.test_dir + now + 'result.html'
@@ -72,7 +115,8 @@ if __name__ == "__main__":
     result = runner.run(discover)
     print('Total test case run: %s' % result.testsRun)
     print('Passed test case run: %s' % result.success_count)
-    print('Failed test case run: %s' % result.failure_count)
+    unpass = int(result.error_count) + int(result.failure_count)
+    print('Failed and error test case run: %d' % unpass)
     print('Detailed test report can be found: %s' % os.path.abspath(filename))
     fp.close()
     print('=====AutoTest End======')
