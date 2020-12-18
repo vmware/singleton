354b69152e866846ff87c9bfcb5dfde8ba71d9bb
huihuiw <huihuiw@vmware.com>
2020-12-18 16:01:14 +0800

    fix CI failure problem: add copyright header for new added file

--- g11n-ws/modules/md-service-about/src/main/java/com/vmware/vip/core/about/exception/AboutAPIException.java
+++ g11n-ws/modules/md-service-about/src/main/java/com/vmware/vip/core/about/exception/AboutAPIException.java
@@ -1,7 +1,14 @@
+/*
+ * Copyright 2019-2020 VMware, Inc.
+ * SPDX-License-Identifier: EPL-2.0
+ */
 package com.vmware.vip.core.about.exception;
 
 import com.vmware.vip.common.exceptions.VIPAPIException;
 
+/**
+ * Exception class for About API
+ */
 public class AboutAPIException extends VIPAPIException {
 
     public AboutAPIException() {
