8a3506d7f1df3c038f3a0cf55170c4cf8c3416dc
zmengjiao <53080608+zmengjiao@users.noreply.github.com>
2021-01-29 15:27:35 +0800

    update copyright for jsclient (#980)

--- cli/load-locale-data.js
+++ cli/load-locale-data.js
@@ -1,7 +1,7 @@
 #!/usr/bin/env node
 
 /*
- * Copyright 2020 VMware, Inc.
+ * Copyright 2019-2021 VMware, Inc.
  * SPDX-License-Identifier: EPL-2.0
  */
 
--- test/plurals.func.spec.ts
+++ test/plurals.func.spec.ts
@@ -1,5 +1,5 @@
 /*
- * Copyright 2020 VMware, Inc.
+ * Copyright 2019-2021 VMware, Inc.
  * SPDX-License-Identifier: EPL-2.0
  */
 import { PLURALFUNCS } from '../src/formatters/plural/plurals.func';
