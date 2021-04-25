1692077249f93ca333d38334d5570c9df60a1801
xiaochao8 <48587632+Xiaochao8@users.noreply.github.com>
2021-04-25 14:18:24 +0800

    Revise CLDR module

---
+++ modules/cldr/coreutil/cache/corecache.go
@@ -0,0 +1,50 @@
+/*
+ * Copyright 2021 VMware, Inc.
+ * SPDX-License-Identifier: EPL-2.0
+ */
+
+package cache
+
+import (
+	"context"
+	"reflect"
+
+	"sgtnserver/internal/cache"
+	"sgtnserver/internal/logger"
+	"sgtnserver/modules/cldr"
+	"sgtnserver/modules/cldr/dao"
+)
+
+var (
+	cldrDao = dao.GetDAO()
+
+	coreDataCache cache.Cache
+)
+
+type coreCache struct{}
+
+func (coreCache) GetCoreData(ctx context.Context, dataType cldr.CoreDataType, data interface{}) (err error) {
+	cacheKey := int(dataType)
+	if dataInCache, err := coreDataCache.Get(cacheKey); err == nil {
+		reflect.ValueOf(data).Elem().Set(reflect.ValueOf(dataInCache))
+		return nil
+	}
+
+	if err = cldrDao.GetCoreData(ctx, dataType, data); err == nil {
+		if cacheErr := coreDataCache.Set(cacheKey, reflect.ValueOf(data).Elem().Interface()); cacheErr != nil {
+			logger.FromContext(ctx).Error(cacheErr.Error())
+		}
+	}
+
+	return err
+}
+func GetCache() coreCache {
+	return coreCache{}
+}
+
+func init() {
+	coreDataCache = cache.NewCache("cldrcore", map[string]interface{}{
+		"MaxCost":     20,
+		"BufferItems": 64,
+	})
+}
--- modules/cldr/dao/dao.go
+++ modules/cldr/dao/dao.go
@@ -20,15 +20,18 @@ import (
 	"go.uber.org/zap"
 )
 
-var json = jsoniter.ConfigDefault
+// this is for contextTransforms, only part of locales have this data. Save result to avoid querying from storage repeatedly
+// var nonexistentMap sync.Map
 
-func GetCoreData(ctx context.Context, dataType CoreDataType, data interface{}) error {
+type cldrDAO struct{}
+
+func (cldrDAO) GetCoreData(ctx context.Context, dataType cldr.CoreDataType, data interface{}) error {
 	log := logger.FromContext(ctx)
 	log.Debug("Read core locale data from storage", zap.String("type", coreDataTypeStrings[dataType]))
 
 	info := getItemInfoOfCoreGroup(dataType)
 	if info.filePath == "" {
-		err := sgtnerror.StatusBadRequest.WithUserMessage(cldr.InvalidDataType, CoreDataTypeNames[dataType])
+		err := sgtnerror.StatusBadRequest.WithUserMessage(cldr.InvalidDataType, coreDataTypeStrings[dataType])
 		log.Error(err.Error())
 		return err
 	}
@@ -40,7 +43,7 @@ func GetCoreData(ctx context.Context, dataType CoreDataType, data interface{}) e
 	return err
 }
 
-func GetLocaleData(ctx context.Context, dataType, locale string, data interface{}) error {
+func (cldrDAO) GetLocaleData(ctx context.Context, locale, dataType string, data interface{}) error {
 	log := logger.FromContext(ctx)
 	log.Debug("Read locale data from storage", zap.String("locale", locale), zap.String("type", dataType))
 
@@ -52,11 +55,15 @@ func GetLocaleData(ctx context.Context, dataType, locale string, data interface{
 	}
 
 	filePath := fmt.Sprintf(info.filePath, locale)
+	// if _, ok := nonexistentMap.Load(filePath); ok {
+	// 	return sgtnerror.StatusNotFound.WithUserMessage("Locale is '%s', type is %v", locale, dataType)
+	// }
 	err := readDataFromBinary(filePath, data, info.jsonPath...)
 	if err != nil {
 		if os.IsNotExist(err) || strings.Contains(err.Error(), "not found") {
 			err = sgtnerror.StatusNotFound.WrapErrorWithMessage(err, filePath)
 			log.Warn(err.Error())
+			// nonexistentMap.Store(filePath, nil)
 		} else {
 			err = sgtnerror.StatusInternalServerError.WrapErrorWithMessage(err, filePath)
 			log.Error(err.Error())
@@ -70,7 +77,7 @@ func readDataFromBinary(filePath string, data interface{}, jsonPath ...interface
 	if err != nil {
 		return err
 	}
-	v := json.Get(bts, jsonPath...)
+	v := jsoniter.ConfigDefault.Get(bts, jsonPath...)
 	if err := v.LastError(); err != nil {
 		return err
 	}
@@ -78,4 +85,8 @@ func readDataFromBinary(filePath string, data interface{}, jsonPath ...interface
 	v.ToVal(data)
 
 	return nil
+}
+
+func GetDAO() cldrDAO {
+	return cldrDAO{}
 }
