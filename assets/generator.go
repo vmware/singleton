e3f17a7613de6a158f2802564e10f1f428a5c5c3
xiaochao <48587632+Xiaochao8@users.noreply.github.com>
2022-01-05 08:04:15 +0000

    change headers to 2022

--- assets/generator.go
+++ assets/generator.go
@@ -1,5 +1,5 @@
 /*
- * Copyright 2021 VMware, Inc.
+ * Copyright 2022 VMware, Inc.
  * SPDX-License-Identifier: EPL-2.0
  */
 //go:generate go-bindata -nomemcopy -pkg bindata  -o ../internal/bindata/bindata.go -ignore=\.go$ ./...
