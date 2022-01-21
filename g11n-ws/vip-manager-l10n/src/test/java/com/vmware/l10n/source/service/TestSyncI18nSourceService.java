637a4cf654b0b0e45a7d890543c01e4ffd4af865
Shi Hu <shihu@vmware.com>
2022-01-21 16:52:19 +0800

    fixed comments

--- g11n-ws/vip-manager-l10n/src/test/java/com/vmware/l10n/source/service/TestSyncI18nSourceService.java
+++ g11n-ws/vip-manager-l10n/src/test/java/com/vmware/l10n/source/service/TestSyncI18nSourceService.java
@@ -33,11 +33,10 @@ public class TestSyncI18nSourceService {
 	private static Logger logger = LoggerFactory.getLogger(TestSyncI18nSourceService.class);
 	@Autowired
 	private WebApplicationContext webApplicationContext;
-	
+
 	@Autowired
 	SyncI18nSourceServiceImpl syncSource;
-	
-	
+
 	@Before
 	public void testBefore() {
 		StringSourceDTO csd = new StringSourceDTO();
@@ -49,7 +48,7 @@ public class TestSyncI18nSourceService {
 		csd.setKey("dc.myhome.open3");
 		csd.setSource("this open3's value");
 		csd.setComment("dc new string");
-		
+
 		SourceServiceImpl source = webApplicationContext.getBean(SourceServiceImpl.class);
 		StringSourceDTO sourceDTO = new StringSourceDTO();
 		sourceDTO.setProductName("test");
@@ -59,60 +58,49 @@ public class TestSyncI18nSourceService {
 		sourceDTO.setKey("dc.myhome.open3");
 		sourceDTO.setSource("this open3's value");
 		sourceDTO.setComment("dc new string");
-		
+
 		try {
 			source.cacheSource(sourceDTO);
 			source.cacheSource(csd);
 			source.writeSourceToCachedFile();
 			List<File> files = DiskQueueUtils.listSourceQueueFile(syncSource.getBasePath());
 			int fileNumb = files.size();
-			
-			if(files != null) {
-				
-				for(File sourceFile: files) {
+
+			if (files != null) {
+
+				for (File sourceFile : files) {
 					try {
 						DiskQueueUtils.moveFile2I18nPath(syncSource.getBasePath(), sourceFile);
 					} catch (IOException e) {
 						logger.error(e.getMessage(), e);
 					}
 				}
-			
+
 			}
-			List<File> i18nQueueFiles = DiskQueueUtils.listQueueFiles(new File(syncSource.getBasePath() + DiskQueueUtils.L10N_TMP_I18N_PATH));
+			List<File> i18nQueueFiles = DiskQueueUtils
+					.listQueueFiles(new File(syncSource.getBasePath() + DiskQueueUtils.L10N_TMP_I18N_PATH));
+			
 			Assert.isTrue(i18nQueueFiles.size() == fileNumb);
 		} catch (Exception e) {
 			logger.error(e.getMessage(), e);
 		}
 	}
 
-	
 	@Test
-	public void test001sendSourceToI18n() {
+	public void test001sendSourceToI18n() throws IOException {
 		SyncI18nSourceServiceImpl syncSource = webApplicationContext.getBean(SyncI18nSourceServiceImpl.class);
 		String basePath = syncSource.getBasePath();
-		
-		
-		Exception ex = null;
-		try {
-			syncSource.sendSourceToI18n();
-			List<File> backFiles = DiskQueueUtils.listQueueFiles(new File(basePath + DiskQueueUtils.L10N_TMP_BACKUP_PATH));
-		    Assert.isTrue(backFiles.size()<1);
-		   
-		    List<File> i18nQueueFiles = DiskQueueUtils.listQueueFiles(new File(syncSource.getBasePath() + DiskQueueUtils.L10N_TMP_I18N_PATH));
-		    for(File delFile : i18nQueueFiles) {
-				DiskQueueUtils.delQueueFile(delFile);
-			}
-		}catch (Exception e) {
-			// TODO Auto-generated catch block
-			logger.error(e.getMessage(), e);
-			ex =e;
-		}
-		Assert.isNull(ex);
-		
-		
-	}
 
-	
-	
+		syncSource.sendSourceToI18n();
+		List<File> backFiles = DiskQueueUtils.listQueueFiles(new File(basePath + DiskQueueUtils.L10N_TMP_BACKUP_PATH));
+		Assert.isTrue(backFiles.size() < 1);
+
+		List<File> i18nQueueFiles = DiskQueueUtils
+				.listQueueFiles(new File(syncSource.getBasePath() + DiskQueueUtils.L10N_TMP_I18N_PATH));
+		for (File delFile : i18nQueueFiles) {
+			DiskQueueUtils.delQueueFile(delFile);
+		}
+
+	}
 
 }
