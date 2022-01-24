7cb3e369a24c730f01b58748c72f86cd91d7f97e
tigershi <shihu@vmware.com>
2022-01-24 13:37:39 +0800

    restructure the update source to local and remote (#1427)
    * add the allow list new feature
    
    * optimize l2 citys
    
    * optimize l2 citys
    
    * optimize l2 citys
    
    * optimize l2 citys
    
    * upgrade the log4j2.16 to log4j2.17
    
    * fixed the NullPointerException issue
    
    * fixed the NullPointerException
    
    * Update Singleton Copyright java file
    
    * Update Singleton Copyright scrpts file
    
    * Update Singleton Copyright scrpts files
    
    * restruct the jobs sync the collection source module
    
    * restruct the jobs sync the collection source module
    
    * process grm send big source content
    
    * process grm send big source content
    
    * format the method
    
    * format the code header
    
    * fixed the bug of restructure of sync data module
    
    * remove the no use class
    
    * update the copyright
    
    * remove the no use class
    
    * add some comment of code
    
    * add the unit test and format code
    
    * add the unit test and format code
    
    * add the unit test and format code
    
    * fixed the comments
    
    * fixed the comments
    
    * fixed the comments
    
    * format the code
    
    * fixed the comments
    
    * fixed the comments
    
    * fixed the comments
    
    * fixed the comments
    
    * fixed comments
    
    * fixed comments
    
    * fixed comments
    
    * fixed the comments

---
+++ g11n-ws/vip-manager-l10n/src/main/java/com/vmware/l10n/crons/SourceLocalBundleCron.java
@@ -0,0 +1,40 @@
+/*
+ * Copyright 2019-2022 VMware, Inc.
+ * SPDX-License-Identifier: EPL-2.0
+ */
+package com.vmware.l10n.crons;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Service;
+
+import com.vmware.l10n.source.service.SyncLocalBundleService;
+
+/**
+ * 
+ * This class used to scheduled merge cached file source to locale bundle
+ *
+ */
+@Service
+@ConditionalOnProperty(value="sync.source.enable", havingValue="true",  matchIfMissing=false)
+public class SourceLocalBundleCron {
+	private final static long THREESECOND = 3000;
+
+	@Autowired
+	private SyncLocalBundleService syncLocalBundleService;
+	
+	/**
+	 * Synchronize the updated source to local bundle or s3 bundle
+	 */
+	@Scheduled(fixedDelay = THREESECOND)
+	public void syncSourceToLocalBundleCron() {
+		syncLocalBundleService.mergeSourceToLocalBundle();
+	}
+
+	
+	
+	
+	
+
+}
