ecf8684fb9a7c634160ea25a6d3aec9e9b82c64f
Colin Lin <47815706+linr211@users.noreply.github.com>
2022-01-25 15:18:37 +0800

    [Ruby client] change the cache's global variables to classic (#1458)
    * update ruby codes(v0.1.5)
    
    * merge v0.1.6 and v0.1.7 codes
    
    * fix scanning issue
    
    * fix rescure issue
    
    * fix scanning issue
    
    * rm history.md
    
    * fix comment
    
    * merge l2 codes
    
    * fix scan issue
    
    * add timezone, add getString_p for plural, bug fix, update sample codes
    
    * add syn for thread-safe
    
    * fix scan issue
    
    * update method name
    
    * bug fix: avoid request for non-existing component
    
    * fix comments
    
    * renew message
    
    * change the cache's global variables to classic

--- lib/sgtn-client/core/cache.rb
+++ lib/sgtn-client/core/cache.rb
@@ -5,11 +5,11 @@ module SgtnClient::Core
             Entry = Struct.new(:expiry, :value)
 
             def self.initialize(disabled=false, opts={})
-                $opts = opts
+                @@opts = opts
                 @mutex = Mutex.new
                 SgtnClient.logger.debug "Initialize cache......"
                 if disabled == false
-                    $data = Hash.new
+                    @@data = Hash.new
                     SgtnClient.logger.debug "Cache is enabled!"
                 else
                     SgtnClient.logger.debug "Cache is disabled!"
@@ -17,74 +17,74 @@ module SgtnClient::Core
             end
 
             def self.keys
-                if $data == nil
+                if @@data == nil
                     return nil
                 end
                 SgtnClient.logger.debug "Get cache keys"
-                $data.keys
+                @@data.keys
             end
 
             def self.get(key)
-                if $data == nil
+                if @@data == nil
                     return nil
                 end
                 SgtnClient.logger.debug "Get cache for key: " + key
                 invalidate
-                $data[key][:value] if has(key)
+                @@data[key][:value] if has(key)
             end
 
             def self.has(key)
-                if $data == nil
+                if @@data == nil
                     return nil
                 end
                 SgtnClient.logger.debug "Has cache for key: " + key
-                $data.has_key? key
+                @@data.has_key? key
             end
 
             def self.put(key, value, ttl=nil)
                 @mutex.synchronize do
-                    if $data == nil
+                    if @@data == nil
                         return nil
                     end
-                    ttl ||= @opts[:ttl]
+                    ttl ||= @@opts[:ttl]
                     # hours from new
                     SgtnClient.logger.debug "Put cache for key '" + key + "' with expired time at'" + (Time.now + ttl*60).to_s
-                    $data[key] = Entry.new(Time.now + ttl*60, value)
+                    @@data[key] = Entry.new(Time.now + ttl*60, value)
                 end
             end
 
             def self.delete(key)
                 @mutex.synchronize do
-                    if $data == nil
+                    if @@data == nil
                         return nil
                     end
                     SgtnClient.logger.debug "Delete cache for key: " + key
-                    $data.delete key
+                    @@data.delete key
                 end
             end
 
             def self.clear
                 @mutex.synchronize do
-                    if $data == nil
+                    if @@data == nil
                         return nil
                     end
                     SgtnClient.logger.debug "Clear cache!"
-                    $data = Hash.new
+                    @@data = Hash.new
                 end
             end
 
             def self.invalidate
                 @mutex.synchronize do
-                    if $data == nil
+                    if @@data == nil
                         return nil
                     end
                     SgtnClient.logger.debug "Invalidating expired cache......"
                     now = Time.now
-                    $data.each {
+                    @@data.each {
                         |k, v|
                             SgtnClient.logger.debug "Checking cache: key=#{k}, expiredtime=#{v[:expiry]}, now=#{now}, expired=#{(v[:expiry] < now)}"
                         }
-                    $data.delete_if {|k, v| v[:expiry] < now}
+                        @@data.delete_if {|k, v| v[:expiry] < now}
                 end
             end
         end
