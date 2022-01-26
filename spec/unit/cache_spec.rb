d62927ef523add30ab231a83a33f44d2b1d96cf5
Colin Lin <linr@vmware.com>
2022-01-26 04:02:43 +0000

    update cache spec

--- spec/unit/cache_spec.rb
+++ spec/unit/cache_spec.rb
@@ -31,18 +31,20 @@ describe SgtnClient do
 
     it "default" do
       env = SgtnClient::Config.default_environment
-      default_language = "default"
       component = "JAVA"
+      default_language = "default"
+      default_cache_key = "test_4.8.1_JAVA_default"
       SgtnClient::Source.loadBundles(default_language)
       config_lang = SgtnClient::Config.configurations[env]["default_language"]
       if config_lang == nil || config_lang == 'en'
-        expect(SgtnClient::CacheUtil.get_cachekey(component, SgtnClient::LocaleUtil.fallback('en-US'))).to eq 'test_4.8.1_JAVA_default'
+        expect(SgtnClient::CacheUtil.get_cachekey(component, SgtnClient::LocaleUtil.fallback('en-US'))).to eq default_cache_key
         expect(SgtnClient::CacheUtil.get_cachekey(component, SgtnClient::LocaleUtil.fallback('de-DE'))).to eq 'test_4.8.1_JAVA_de'
       end
+      # reset the default language to 'zh-Hans'
       SgtnClient::Config.configurations[env]["default_language"] = 'zh-Hans'
+      expect(SgtnClient::CacheUtil.get_cachekey(component, SgtnClient::LocaleUtil.fallback('zh-Hans'))).to eq default_cache_key
+      expect(SgtnClient::CacheUtil.get_cachekey(component, SgtnClient::LocaleUtil.fallback('zh-CN'))).to eq default_cache_key
       expect(SgtnClient::CacheUtil.get_cachekey(component, SgtnClient::LocaleUtil.fallback('en-US'))).to eq 'test_4.8.1_JAVA_en'
-      expect(SgtnClient::CacheUtil.get_cachekey(component, SgtnClient::LocaleUtil.fallback('zh-Hans'))).to eq 'test_4.8.1_JAVA_default'
-      expect(SgtnClient::CacheUtil.get_cachekey(component, SgtnClient::LocaleUtil.fallback('zh-CN'))).to eq 'test_4.8.1_JAVA_default'
     end
 
   end
