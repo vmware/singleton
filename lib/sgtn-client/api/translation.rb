6f322b8080c129ec88a04cc58250bdfe86d1adc6
Colin Lin <linr@vmware.com>
2022-03-02 05:37:12 +0000

    remove service.rb

--- lib/sgtn-client/api/translation.rb
+++ lib/sgtn-client/api/translation.rb
@@ -18,7 +18,7 @@ module SgtnClient
 
       def self.getString(component, key, locale)
         SgtnClient.logger.debug "[Translation.getString]component: #{component}, key: #{key}, locale: #{locale}"
-        str = SgtnClient::ServiceUtil.get_translation(component, key, locale)
+        str = getTranslation(component, key, locale)
         if str.nil?
           str = SgtnClient::Source.getSource(component, key)
           if str.nil?
@@ -27,7 +27,7 @@ module SgtnClient
         else
           unless LocaleUtil.is_source_locale(locale)
             source = Source.getSource(component, key)
-            str = ServiceUtil.compare_source(component, key, source, str)
+            str = compare_source(component, key, source, str)
           end
         end
         str
@@ -35,7 +35,7 @@ module SgtnClient
 
       def self.getString_p(component, key, plural_args, locale)
         SgtnClient.logger.debug "[Translation][getString_p]component=#{component}, key=#{key}, locale=#{locale}"
-        str = SgtnClient::ServiceUtil.get_translation(component, key, locale)
+        str = getTranslation(component, key, locale)
         if str.nil?
           str = SgtnClient::Source.getSource(component, key)
           if str.nil?
@@ -67,7 +67,7 @@ module SgtnClient
       def self.getStrings(component, locale)
         SgtnClient.logger.debug "[Translation][getStrings]component=#{component}, locale=#{locale}"
         locale = SgtnClient::LocaleUtil.get_best_locale(locale)
-        items = SgtnClient::ServiceUtil.get_cs(component, locale)
+        items = get_cs(component, locale)
         default = SgtnClient::Config.configurations.default
         if items.nil? || items["messages"] == nil
           items = {}
@@ -83,5 +83,89 @@ module SgtnClient
         end
         return items
        end
-  end
-end
+
+
+       private
+ 
+       def self.getTranslation(component, key, locale)
+         locale = SgtnClient::LocaleUtil.get_best_locale(locale)
+         items = get_cs(component, locale)
+         if items.nil? || items["messages"] == nil
+           nil
+         else
+           items["messages"][key]
+         end
+       end
+ 
+       def self.get_cs(component, locale)
+        cache_key = SgtnClient::CacheUtil.get_cachekey(component, locale)
+        SgtnClient.logger.debug "[ServiceUtil][get_cs]cache_key=#{cache_key}"
+        expired, items = SgtnClient::CacheUtil.get_cache(cache_key)
+        if items.nil? || expired
+          items = load(component, locale)
+          if items.nil?
+            items = SgtnClient::Source.getSources(component, SgtnClient::Config.configurations.default)
+            SgtnClient::Core::Cache.put(cache_key, items, 60)
+          else
+            SgtnClient::CacheUtil.write_cache(cache_key, items)
+          end
+        else
+          SgtnClient.logger.debug "[ServiceUtil]get translations from cache with key: " + cache_key
+        end
+
+        return items
+       end
+ 
+       def self.load(component, locale)
+         env = SgtnClient::Config.default_environment
+         mode = SgtnClient::Config.configurations[env]["bundle_mode"]
+         SgtnClient.logger.debug "[Translation][load]mode=#{mode}"
+         if mode == 'offline'
+           return load_o(component, locale)
+         else
+           return load_s(component, locale)
+         end
+       end
+ 
+       def self.load_o(component, locale)
+         env = SgtnClient::Config.default_environment
+         product_name = SgtnClient::Config.configurations[env]["product_name"]
+         version = SgtnClient::Config.configurations[env]["version"].to_s
+         translation_bundle = SgtnClient::Config.configurations[env]["translation_bundle"]
+         bundlepath = translation_bundle + "/" + product_name + "/" + version + "/" + component + "/messages_" + locale + ".json"
+         SgtnClient::FileUtil.read_json(bundlepath)
+       end
+ 
+       def self.load_s(component, locale)
+         env = SgtnClient::Config.default_environment
+         product_name = SgtnClient::Config.configurations[env]["product_name"]
+         vip_server = SgtnClient::Config.configurations[env]["vip_server"]
+         version = SgtnClient::Config.configurations[env]["version"].to_s
+         url = vip_server + "/i18n/api/v2/translation/products/" + product_name + "/versions/" + version + "/locales/" + locale + "/components/" + component+ "?checkTranslationStatus=false&machineTranslation=false&pseudo=false"
+         begin
+           obj = SgtnClient::Core::Request.get(url)
+         rescue => exception
+           SgtnClient.logger.error exception.message
+         end
+         if obj != nil
+           obj = obj["data"]
+         end
+         return obj
+       end
+
+       # Compare local source with remote source
+       def self.compare_source(component, key, source, translation)
+         source_locale = SgtnClient::LocaleUtil.get_source_locale
+         SgtnClient.logger.debug "[ServiceUtil][compare_source]component=#{component},key=#{key},default_locale=#{source_locale},source=#{source},translation=#{translation}"
+         items = get_cs(component, source_locale)
+         if items.nil? || items["messages"].nil?
+           translation
+         else
+           target = items["messages"][key]
+           source == target ? translation : source
+         end
+       end
+ 
+   end
+ 
+ end
