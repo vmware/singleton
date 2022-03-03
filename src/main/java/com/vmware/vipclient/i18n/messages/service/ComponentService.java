7c02f910e284d07540a506ade3bc48c10ec9c733
JESSIELY JUACHON <jjuachon@vmware.com>
2022-03-03 12:17:48 -0800

    Update src/main/java/com/vmware/vipclient/i18n/messages/service/ComponentService.java
    Co-authored-by: Xiaochao Li <48587632+Xiaochao8@users.noreply.github.com>

--- src/main/java/com/vmware/vipclient/i18n/messages/service/ComponentService.java
+++ src/main/java/com/vmware/vipclient/i18n/messages/service/ComponentService.java
@@ -157,7 +157,7 @@ public class ComponentService {
 		CacheService cacheService = new CacheService(dto);
 
 		// Allow only one thread to create the new cache item
-		synchronized (TranslationCacheManager.getInstance().getCache(VIPCfg.CACHE_L3)) {
+		synchronized (TranslationCacheManager.getCache(VIPCfg.CACHE_L3)) {
 			MessageCacheItem cacheItem = cacheService.getCacheOfComponent();
 			if (cacheItem == null) { // If the cache item hasn't been created by any other thread
 				cacheItem = new MessageCacheItem(); // Create a new cacheItem object to be stored in cache
