ffa292ca5c88b4c0a45684f72523572190f846c7
xiaochao <48587632+Xiaochao8@users.noreply.github.com>
2022-03-08 11:12:12 +0000

    keep cache

--- lib/sgtn-client/core/cache.rb
+++ lib/sgtn-client/core/cache.rb
@@ -81,14 +81,8 @@ module SgtnClient::Core
                     now = Time.now
                     if has(key)
                         v = @@data[key]
-                        expired = false
+                        expired = v[:expiry] < now
                         SgtnClient.logger.debug "[Cache][invalidate]check cache: key=#{key}, expiredtime=#{v[:expiry]}, now=#{now}, expired=#{(v[:expiry] < now)}"
-                        if v[:expiry] < now
-                            SgtnClient.logger.debug "[Cache][invalidate]before deleting the cache: data=#{@@data}"
-                            @@data.delete(key)
-                            SgtnClient.logger.debug "[Cache][invalidate]after deleting the cache: data=#{@@data}"
-                            expired = true
-                        end
                         return expired, v[:value]
                     else
                         return nil, nil
