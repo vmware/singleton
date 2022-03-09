65acabeab2e625356bee8ad246a81e4d8e431c35
Xiaochao Li <48587632+Xiaochao8@users.noreply.github.com>
2022-03-08 23:55:17 -0800

    add license headers (#1581)

--- lib/sgtn-client/cldr/core_ext.rb
+++ lib/sgtn-client/cldr/core_ext.rb
@@ -1,3 +1,6 @@
+# Copyright 2022 VMware, Inc.
+# SPDX-License-Identifier: EPL-2.0
+
 require 'sgtn-client/cldr/localized_datetime'
 require 'sgtn-client/cldr/localized_date'
 require 'sgtn-client/cldr/localized_time'
