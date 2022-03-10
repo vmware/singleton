ff249e8407ec43496c6771a23b667c38945b8f3e
xiaochao <48587632+Xiaochao8@users.noreply.github.com>
2022-03-10 12:56:19 +0000

    add logs

--- lib/sgtn-client/api/translation.rb
+++ lib/sgtn-client/api/translation.rb
@@ -1,138 +1,134 @@
-# 
+#
 #  Copyright 2019-2022 VMware, Inc.
 #  SPDX-License-Identifier: EPL-2.0
 #
 
 require 'multi_json'
+require 'weakref'
 
 module SgtnClient
-
   module Core
-    autoload :Request,     "sgtn-client/core/request"
-    autoload :Cache,       "sgtn-client/core/cache"
-    autoload :CacheUtil,   "sgtn-client/util/cache-util"
-    autoload :LocaleUtil,   "sgtn-client/util/locale-util"
+    autoload :Request,     'sgtn-client/core/request'
+    autoload :Cache,       'sgtn-client/core/cache'
+    autoload :CacheUtil,   'sgtn-client/util/cache-util'
+    autoload :LocaleUtil, 'sgtn-client/util/locale-util'
   end
-  
+
   class Translation
+    def self.getString(component, key, locale)
+      SgtnClient.logger.debug "[Translation.getString]component: #{component}, key: #{key}, locale: #{locale}"
+      translation = getTranslation(component, key, locale)
+      if translation.nil? && !LocaleUtil.is_source_locale(locale)
+        translation = getTranslation(component, key, LocaleUtil.get_source_locale)
+      end
+      translation
+    end
 
-      def self.getString(component, key, locale)
-        SgtnClient.logger.debug "[Translation.getString]component: #{component}, key: #{key}, locale: #{locale}"
-        translation = getTranslation(component, key, locale)
-        if translation.nil? && !LocaleUtil.is_source_locale(locale)
+    def self.getString_p(component, key, plural_args, locale)
+      SgtnClient.logger.debug "[Translation][getString_p]component=#{component}, key=#{key}, locale=#{locale}"
+      translation = getTranslation(component, key, locale)
+      if translation.nil?
+        unless LocaleUtil.is_source_locale(locale)
           translation = getTranslation(component, key, LocaleUtil.get_source_locale)
+          translation.to_plural_s(LocaleUtil.get_source_locale.to_sym, plural_args) if translation
         end
-        translation
+      else
+        translation.to_plural_s(locale, plural_args)
+      end
+    end
+
+    def self.getString_f(component, key, args, locale, *optionals)
+      SgtnClient.logger.debug "[Translation][getString_f]component=#{component}, key=#{key}, locale=#{locale}"
+      s = getString(component, key, locale, *optionals)
+      return nil if s.nil?
+
+      if args.is_a?(Hash)
+        args.each do |source, arg|
+          s.gsub! "{#{source}}", arg
+        end
+      elsif args.is_a?(Array)
+        s = SgtnClient.logger.info { sprintf(s % args) }
+      end
+      s
+    end
+
+    def self.getStrings(component, locale)
+      SgtnClient.logger.debug "[Translation][getStrings]component=#{component}, locale=#{locale}"
+      locale = SgtnClient::LocaleUtil.get_best_locale(locale)
+      items = get_cs(component, locale)
+      if (items.nil? || items['messages'].nil?) && !LocaleUtil.is_source_locale(locale)
+        items = get_cs(component, LocaleUtil.get_source_locale)
       end
 
-      def self.getString_p(component, key, plural_args, locale)
-        SgtnClient.logger.debug "[Translation][getString_p]component=#{component}, key=#{key}, locale=#{locale}"
-        translation = getTranslation(component, key, locale)
-        if translation.nil?
-          if !LocaleUtil.is_source_locale(locale)
-            translation = getTranslation(component, key, LocaleUtil.get_source_locale)
-            translation.to_plural_s(LocaleUtil.get_source_locale.to_sym, plural_args) if translation
-          end
-        else
-          translation.to_plural_s(locale, plural_args)
-        end
-      end
+      items
+    end
 
-      def self.getString_f(component, key, args, locale, *optionals)
-         SgtnClient.logger.debug "[Translation][getString_f]component=#{component}, key=#{key}, locale=#{locale}"
-         s = getString(component, key, locale, *optionals)
-         if s.nil?
-          return nil
-         end
-         if args.is_a?(Hash)
-          args.each do |source, arg|
-            s.gsub! "{#{source}}", arg
-          end
-         elsif args.is_a?(Array)
-          s = sprintf s % args
-         end
-         return s
-      end
+    private
 
-      def self.getStrings(component, locale)
-        SgtnClient.logger.debug "[Translation][getStrings]component=#{component}, locale=#{locale}"
-        locale = SgtnClient::LocaleUtil.get_best_locale(locale)
-        items = get_cs(component, locale)
-        if (items.nil? || items["messages"].nil?) && !LocaleUtil.is_source_locale(locale)
-          items = get_cs(component, LocaleUtil.get_source_locale)
-        end
+    def self.getTranslation(component, key, locale)
+      locale = SgtnClient::LocaleUtil.get_best_locale(locale)
+      items = get_cs(component, locale)
+      items&.dig('messages', key)
+    end
 
-        return items
-       end
-
-
-      private
-
-      def self.getTranslation(component, key, locale)
-        locale = SgtnClient::LocaleUtil.get_best_locale(locale)
-        items = get_cs(component, locale)
-        items&.dig("messages", key)
-      end
-
-      def self.get_cs(component, locale)
-        cache_key = SgtnClient::CacheUtil.get_cachekey(component, locale)
-        SgtnClient.logger.debug "[Translation][get_cs]cache_key=#{cache_key}"
-        expired, items = SgtnClient::CacheUtil.get_cache(cache_key)
-        if items.nil? || expired
-          if items.nil?
-            items = single_refresh(component, locale).value
-          else
-            if locale != LocaleUtil.get_source_locale # local source never expires.
-              single_refresh(component, locale)
-            end
-          end
-        else
-          SgtnClient.logger.debug "[Translation]get translations from cache with key: " + cache_key
-        end
-
-        return items
-       end
-
-      def self.load(component, locale)
-        env = SgtnClient::Config.default_environment
-        mode = SgtnClient::Config.configurations[env]["bundle_mode"]
-        SgtnClient.logger.debug "[Translation][load]mode=#{mode}"
-        if mode == 'offline'
-          return load_o(component, locale)
-        else
-          return load_s(component, locale)
+    def self.get_cs(component, locale)
+      cache_key = SgtnClient::CacheUtil.get_cachekey(component, locale)
+      SgtnClient.logger.debug "[Translation][get_cs]cache_key=#{cache_key}"
+      expired, items = SgtnClient::CacheUtil.get_cache(cache_key)
+      if items.nil?
+        items = single_refresh(component, locale).value # refresh synchronously if not in cache
+      elsif expired && locale != LocaleUtil.get_source_locale # local source never expires.
+          single_refresh(component, locale)  # refresh in background
         end
       end
 
-      def self.load_o(component, locale)
-        env = SgtnClient::Config.default_environment
-        product_name = SgtnClient::Config.configurations[env]["product_name"]
-        version = SgtnClient::Config.configurations[env]["version"].to_s
-        translation_bundle = SgtnClient::Config.configurations[env]["translation_bundle"]
-        bundlepath = translation_bundle + "/" + product_name + "/" + version + "/" + component + "/messages_" + locale + ".json"
-        SgtnClient::FileUtil.read_json(bundlepath)
-      end
+      items
+    end
 
-      def self.load_s(component, locale)
-        env = SgtnClient::Config.default_environment
-        product_name = SgtnClient::Config.configurations[env]["product_name"]
-        vip_server = SgtnClient::Config.configurations[env]["vip_server"]
-        version = SgtnClient::Config.configurations[env]["version"].to_s
-        url = vip_server + "/i18n/api/v2/translation/products/" + product_name + "/versions/" + version + "/locales/" + locale + "/components/" + component+ "?checkTranslationStatus=false&machineTranslation=false&pseudo=false"
-        begin
-          obj = SgtnClient::Core::Request.get(url)
-        rescue => exception
-          SgtnClient.logger.error exception.message
-        end
-        obj && obj["data"]
+    def self.load(component, locale)
+      env = SgtnClient::Config.default_environment
+      mode = SgtnClient::Config.configurations[env]['bundle_mode']
+      SgtnClient.logger.debug "[Translation][load]mode=#{mode}"
+      if mode == 'offline'
+        load_o(component, locale)
+      else
+        load_s(component, locale)
       end
+    end
+
+    def self.load_o(component, locale)
+      env = SgtnClient::Config.default_environment
+      product_name = SgtnClient::Config.configurations[env]['product_name']
+      version = SgtnClient::Config.configurations[env]['version'].to_s
+      translation_bundle = SgtnClient::Config.configurations[env]['translation_bundle']
+      bundlepath = translation_bundle + '/' + product_name + '/' + version + '/' + component + '/messages_' + locale + '.json'
+      SgtnClient::FileUtil.read_json(bundlepath)
+    end
+
+    def self.load_s(component, locale)
+      env = SgtnClient::Config.default_environment
+      product_name = SgtnClient::Config.configurations[env]['product_name']
+      vip_server = SgtnClient::Config.configurations[env]['vip_server']
+      version = SgtnClient::Config.configurations[env]['version'].to_s
+      url = vip_server + '/i18n/api/v2/translation/products/' + product_name + '/versions/' + version + '/locales/' + locale + '/components/' + component + '?checkTranslationStatus=false&machineTranslation=false&pseudo=false'
+      begin
+        obj = SgtnClient::Core::Request.get(url)
+      rescue StandardError => e
+        SgtnClient.logger.error e.message
+      end
+      obj && obj['data']
+    end
 
     def self.load_and_compare_source(component, locale)
       source_cache_key = SgtnClient::CacheUtil.get_cachekey(component, LocaleUtil.get_source_locale)
-      _, source_bundle = SgtnClient::CacheUtil.get_cache(source_cache_key)
+      expired, source_bundle = SgtnClient::CacheUtil.get_cache(source_cache_key)
       if source_bundle.nil? || source_bundle.empty?
+        SgtnClient.logger.info { message = source_bundle.nil? ? 'Source is nil' : 'Source is empty'
+          sprintf("%-18s %-16s %-15s %s", Thread.current.name, "[#{__method__}]", "[#{component}.#{locale}]", message) }
         source_bundle = single_load(component, SgtnClient::Config.configurations.default).value
         SgtnClient::CacheUtil.write_cache(source_cache_key, source_bundle)
+      else
+        SgtnClient.logger.info { sprintf("%-18s %-16s %-15s %s", Thread.current.name, "[#{__method__}]", "[#{component}.#{locale}]", "Source isn't empty and expired: #{expired}") }
       end
       return source_bundle if LocaleUtil.is_source_locale(locale)
 
@@ -168,14 +164,22 @@ module SgtnClient
       @@load_threads_lock.synchronize do
         thread = @@load_threads[cache_key]
         if thread.nil? || thread.alive? == false
-          thread = Thread.new do
-            if locale == SgtnClient::Config.configurations.default
+          SgtnClient.logger.info { sprintf("%-18s %-16s %-15s %s", Thread.current.name, "[#{__method__}]", "[#{component}.#{locale}]", "Starting a new thread") }
+          thread = Thread.new(Thread.current.name) do |parent_thread_name|
+            Thread.current.name = (0...3).map { (65 + rand(26)).chr }.join
+            Thread.current.name = "#{parent_thread_name}:#{Thread.current.name}"
+            SgtnClient.logger.info { sprintf("%-18s %-16s %-15s %s", Thread.current.name, "[#{__method__}]", "[#{component}.#{locale}]", "Thread is started") }
+            bundle = if locale == SgtnClient::Config.configurations.default
               Source.getBundle(component, SgtnClient::Config.configurations.default)
             else
               load(component, locale)
             end
+            SgtnClient.logger.info { sprintf("%-18s %-16s %-15s %s", Thread.current.name, "[#{__method__}]", "[#{component}.#{locale}]", "Thread is going to end") }
+            bundle
           end
           @@load_threads[cache_key] = thread
+        else
+          SgtnClient.logger.info { sprintf("%-18s %-16s %-15s %s", Thread.current.name, "[#{__method__}]", "[#{component}.#{locale}]", "Another thread is alive") }
         end
         return thread
       end
@@ -190,13 +194,24 @@ module SgtnClient
         if thread.nil? || thread.alive? == false
           expired, items = SgtnClient::CacheUtil.get_cache(cache_key)
           if expired || items.nil?
-            thread = Thread.new do
+            SgtnClient.logger.info {message = expired ? "Cache expired" : "Initialize cache"
+               sprintf("%-18s %-16s %-15s %s", Thread.current.name, "[#{__method__}]", "[#{component}.#{locale}]", "Starting a new thread: #{message}") }
+            thread = Thread.new(Thread.current.name) do |parent_thread_name|
+              Thread.current.name = (0...3).map { (65 + rand(26)).chr }.join
+              Thread.current.name = "#{parent_thread_name}:#{Thread.current.name}"
+              SgtnClient.logger.info { sprintf("%-18s %-16s %-15s %s", Thread.current.name, "[#{__method__}]", "[#{component}.#{locale}]", "Thread is started") }
               items = load_and_compare_source(component, locale)
               SgtnClient::CacheUtil.write_cache(cache_key, items) if items&.empty? == false
+
+              SgtnClient.logger.info { sprintf("%-18s %-16s %-15s %s", Thread.current.name, "[#{__method__}]", "[#{component}.#{locale}]", "Thread is going to end") }
               items
             end
             @@refresh_threads[cache_key] = thread
+          else
+            SgtnClient.logger.info { sprintf("%-18s %-16s %-15s %s", Thread.current.name, "[#{__method__}]", "[#{component}.#{locale}]", " Not expired") }
           end
+        else
+          SgtnClient.logger.info { sprintf("%-18s %-16s %-15s %s", Thread.current.name, "[#{__method__}]", "[#{component}.#{locale}]", "Another thread is alive") }
         end
         return thread
       end
