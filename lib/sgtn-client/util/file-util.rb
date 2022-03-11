dc4595bb47f7f321d5d1f5c5b956a9099d065ee4
JESSIELY JUACHON <jessiejuachon@gmail.com>
2022-03-11 00:41:20 -0800

    Fix #1575 File read does not support thread concurrency (#1593)
    Co-authored-by: Xiaochao Li <48587632+Xiaochao8@users.noreply.github.com>

--- lib/sgtn-client/util/file-util.rb
+++ lib/sgtn-client/util/file-util.rb
@@ -7,31 +7,19 @@ require 'yaml'
 module SgtnClient
 
   class FileUtil
-
-      @mutex = Mutex.new
-
       def self.read_json(bundlepath)
         SgtnClient.logger.debug "[FileUtil]read json file from: " + bundlepath
-        @mutex.synchronize do
-          data_hash = nil
-          begin
-            file = File.read(bundlepath)
-            data_hash = MultiJson.load(file)
-          rescue => exception
-            SgtnClient.logger.error exception.message
-          end
-          return data_hash
+        begin
+          return MultiJson.load(File.read(bundlepath))
+        rescue => exception
+          SgtnClient.logger.error exception.message
         end
+        nil
       end
 
       def self.read_yml(file_name)
-        SgtnClient.logger.debug "[FileUtil]read yml file from: " + file_name
-        @mutex.synchronize do
-          erb = ERB.new(File.read(file_name))
-          erb.filename = file_name
-          YAML.load(erb.result)
-        end
+        YAML::load(File.read(file_name))
       end
   end
-
 end
+
