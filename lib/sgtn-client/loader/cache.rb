af519d0c0ed7e0f1d3306d47eff9551868d93827
JESSIELY JUACHON <jjuachon@vmware.com>
2022-03-28 16:11:53 -0700

    Update lib/sgtn-client/loader/cache.rb
    Debug log

--- lib/sgtn-client/loader/cache.rb
+++ lib/sgtn-client/loader/cache.rb
@@ -12,6 +12,7 @@ module SgtnClient::TranslationLoader::Cache
     cache_item = SgtnClient::CacheUtil.get_cache(cache_key)
     if cache_item.nil?
       # refresh synchronously if not in cache
+      SgtnClient.logger.debug "[#{self.to_s}][#{__FILE__}][#{__method__}] Cache miss. cache_key=#{cache_key}"
       cache_item = (single_loader { |c, l| super(c, l) }).operate(cache_key, component, locale).value 
       # TODO: if an error occurs when requesting a bundle, need to avoid more requests
     elsif SgtnClient::CacheUtil.is_expired(cache_item) && locale != SgtnClient::LocaleUtil.get_source_locale # local source never expires.
