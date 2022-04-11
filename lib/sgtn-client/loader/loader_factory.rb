a82c605278534f22b833bb22fceb83422e7d8c8e
xiaochao <48587632+Xiaochao8@users.noreply.github.com>
2022-04-11 07:44:15 +0000

    Singleton server takes precedence over local translation

--- lib/sgtn-client/loader/loader_factory.rb
+++ lib/sgtn-client/loader/loader_factory.rb
@@ -14,9 +14,8 @@ module SgtnClient::TranslationLoader
     def self.create(config)
       loaders = []
       loaders << Source.new if config['source_bundle']
-      # local translation takes precedence over remote
-      loaders << LocalTranslation.new if config['translation_bundle']
       loaders << SgtnServer.new if config['vip_server']
+      loaders << LocalTranslation.new if config['translation_bundle']
       raise SingletonError, 'No translation is available!' if loaders.empty?
 
       chain_loader = Class.new(Chain)
