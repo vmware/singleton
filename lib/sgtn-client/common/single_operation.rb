e21e41fc8fa62970b8d11838311266dc1bd37f90
xiaochao <48587632+Xiaochao8@users.noreply.github.com>
2022-04-13 02:26:56 +0000

    change comments

--- lib/sgtn-client/common/single_operation.rb
+++ lib/sgtn-client/common/single_operation.rb
@@ -13,14 +13,13 @@ module SgtnClient
       @creator = block
     end
 
-    # return new created object
+    # return new created object(return nil possibly)
     def operate(id, *args)
       @lock.synchronize do
         obj = @hash[id]
         @conditions.each do |con|
           return obj unless con.call(id, obj, *args)
         end
-        # TODO: whatif returning nil
         @hash[id] = @creator.call(id, obj, *args)
       end
     end
