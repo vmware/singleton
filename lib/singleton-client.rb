0874103cad174cef33f004c31e5978e7d539d0b8
xiaochao <48587632+Xiaochao8@users.noreply.github.com>
2022-04-18 02:02:36 +0000

    ccc

--- lib/singleton-client.rb
+++ lib/singleton-client.rb
@@ -18,11 +18,11 @@ end
 module Singleton # :nodoc:
   module Base # :nodoc:
     # Gets configuration object.
-    def load_config(config_file = "./config/singleton.yml")
-      SgtnClient.load(config_file, SgtnClient::Config.default_environment)
+    def load_config(*args)
+      SgtnClient.load(*args)
     end
 
-    def translate(key, component: component, locale: nil, **kwargs)
+    def translate(key, component: nil, locale: nil, **kwargs)
       translate!(key, component: component, locale: locale, **kwargs)
     rescue StandardError => e
       SgtnClient.logger.error "couldn't translate #{key}, #{component}, #{locale}. error: #{e}"
@@ -31,7 +31,7 @@ module Singleton # :nodoc:
     alias t translate
 
     # raise error when translation is not found
-    def translate!(key, component: component, locale: nil, **kwargs)
+    def translate!(key, component: nil, locale: nil, **kwargs)
       SgtnClient.logger.debug "[#{method(__callee__).owner}.#{__callee__}] key: #{key}, component: #{component}, locale: #{locale}, args: #{kwargs}"
 
       locale = locale.nil? ? self.locale : LocaleUtil.get_best_locale(locale)
@@ -48,34 +48,19 @@ module Singleton # :nodoc:
     end
     alias t! translate!
 
-    def get_translations(component: component, locale: nil)
+    def get_translations(component: nil, locale: nil)
       get_translations(component, locale)
     rescue StandardError => e
       SgtnClient.logger.error "couldn't get translations for component: #{component}, locale: #{locale}. error: #{e}"
       { 'component' => component, 'locale' => locale, 'messages' => {} }
     end
 
-    def get_translations!(component: component, locale: nil)
+    def get_translations!(component: nil, locale: nil)
       SgtnClient.logger.debug "[#{method(__callee__).owner}.#{__callee__}] component: #{component}, locale: #{locale}"
 
       locale = locale.nil? ? self.locale : LocaleUtil.get_best_locale(locale)
 
       { 'component' => component, 'locale' => locale, 'messages' => get_bundle(component, locale) || {} }
-    end
-
-    # Executes block with given locale set.
-    def with_locale(tmp_locale = nil)
-      if tmp_locale.nil?
-        yield
-      else
-        current_locale = locale
-        self.locale = tmp_locale
-        begin
-          yield
-        ensure
-          self.locale = current_locale
-        end
-      end
     end
 
     def locale
@@ -91,17 +76,11 @@ module Singleton # :nodoc:
     def get_bundle(component, locale)
       id = Common::BundleID.new(component, locale)
       bundles = Config.available_bundles
-      unless bundles.nil? || bundles.empty? || bundles.include?(id)
-        raise SingletonError "bundle is unavailable. #{id}"
-      end
+      raise SingletonError "bundle is unavailable. #{id}" unless bundles.nil? || bundles.empty? || bundles.include?(id)
 
       Config.loader.get_bundle(component, locale)
     end
   end
 
   extend Base
-end
-
-module Sgtn # :nodoc:
-  extend Singleton::Base
 end
