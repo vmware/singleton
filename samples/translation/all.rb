c8aeb5870a835e95eb35589da1f4a1fd7d8d7956
Xiaochao Li <48587632+Xiaochao8@users.noreply.github.com>
2022-04-28 18:34:18 -0700

    update README and sample application (#1674)

--- samples/translation/all.rb
+++ samples/translation/all.rb
@@ -1,21 +1,59 @@
-require 'singleton-ruby'
+# Copyright 2022 VMware, Inc.
+# SPDX-License-Identifier: EPL-2.0
 
-include SgtnClient
+require 'singleton-client'
 
 # load config file to initialize app
-SgtnClient.load("./config/sgtnclient.yml", "test")
+Singleton.load_config('./config/sgtnclient.yml', 'test')
 
-# load english bundle for fallback
-SgtnClient::Source.loadBundles("default")
+component = 'JAVA'
+locale = 'zh-CN'
+en_locale = 'en'
 
-# get translation
-#@Result = SgtnClient::Translation.getString("JAVA", "helloworld", "zh-Hans")
+normal_cases = {}
+plural_cases = {}
+formatting_cases = {}
+setting_locale = {}
+string_translation = { 'normal' => normal_cases, 'plural' => plural_cases, 'formatting' => formatting_cases, 'setting locale' => setting_locale }
 
-# get non-existing translation
-puts SgtnClient::Translation.getString("JAVA", "hello", "zh-Hans")
+# translate a string
+normal_cases['translate a string'] = Singleton.t('helloworld', component, locale)
 
-# get pluralized translation
-#@Result = SgtnClient::Translation.getString_p("JAVA", "plural_key", { :cat_count => 1 }, "zh-Hans")
+# translate a string
+normal_cases['translate an English string'] = Singleton.t('helloworld', component, en_locale)
 
-# get formatting translation
-#@Result = SgtnClient::Translation.getString_f("JAVA", "welcome", ["虚拟世界", "机器人"], "zh-Hans")
+# translate a string only in source
+normal_cases['translate a string only in source'] = Singleton.t('hello', component, locale)
+
+# translate a non-existing string
+normal_cases['translate a nonexistent string returning key'] = Singleton.t('nonexistent', component, locale)
+normal_cases['translate a nonexistent string with default value'] = Singleton.t('nonexistent', component, locale) { 'default value when translation is missing' }
+
+# translate a string with pluralization
+plural_cases['translate a plural string - 0'] = Singleton.t('plural_key', component, locale, cat_count: 0, place: '房间')
+plural_cases['translate a plural string - 1'] = Singleton.t('plural_key', component, locale, cat_count: 1, place: '盒子')
+plural_cases['translate a plural string - 2'] = Singleton.t('plural_key', component, locale, cat_count: 2, place: 'bush')
+plural_cases['-----------------------'] = ''
+plural_cases['translate an English plural string - 0'] = Singleton.t('plural_key', component, en_locale, cat_count: 0, place: 'room')
+plural_cases['translate an English plural string - 1'] = Singleton.t('plural_key', component, en_locale, cat_count: 1, place: 'box')
+plural_cases['translate an English plural string - 2'] = Singleton.t('plural_key', component, en_locale, cat_count: 2, place: 'bush')
+
+# format translation
+formatting_cases["translate a string with placeholders - #{locale}"] = Singleton.t('welcome', component, locale, place: '虚拟世界', name: '机器人')
+formatting_cases["translate a string with placeholders - #{en_locale}"] = Singleton.t('welcome', component, en_locale, place: '虚拟世界', name: '机器人')
+
+# set locale
+Singleton.locale = locale
+setting_locale["set loale to #{locale} before translating"] = Singleton.t('helloworld', component)
+Singleton.locale = en_locale
+setting_locale["set loale to #{en_locale} before translating"] = Singleton.t('helloworld', component)
+
+bundle_translations = {}
+bundle_translations["#{locale} translations"] = Singleton.get_translations(component, locale)
+bundle_translations['English translations'] = Singleton.get_translations(component, en_locale)
+Singleton.locale = locale
+bundle_translations["set loale to #{locale}"] = Singleton.get_translations(component)
+
+@Result = { 'string translation' => string_translation, 'bundle translations' => bundle_translations }
+
+pp @Result
