f3055f3737f5cf2c64e846727167493e5b4d99f5
xiaochao <48587632+Xiaochao8@users.noreply.github.com>
2022-05-30 01:17:58 +0000

    ccc

--- spec/support/utils.rb
+++ spec/support/utils.rb
@@ -14,3 +14,25 @@ def expire_cache(id)
   cache_item = SgtnClient::CacheUtil.get_cache(id)
   cache_item[:expiry] = Time.now
 end
+
+def extract_arguments(trace)
+  param_names = trace.parameters.map(&:last)
+  param_names.map { |n| [n, trace.binding.eval(n.to_s)] }.to_h
+end
+
+def extract_locals(trace)
+  local_names = trace.binding.local_variables
+  local_names.map { |n|
+    [n, trace.binding.local_variable_get(n)]
+  }.to_h
+end
+
+def traverse_modules(m)
+  m.constants(false).each do |c|
+    const = root.const_get(c, false)
+    if [Class, Module].include? const.class
+      const.methods.each { |method| yield method }
+      traverse_modules(const)
+    end
+  end
+end
