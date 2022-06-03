8264220a0b63e86b8b2e68dca84f817cbc5d212e
Xiaochao Li <48587632+Xiaochao8@users.noreply.github.com>
2022-06-03 08:38:10 +0800

    Update lib/sgtn-client/i18n_backend.rb
    Co-authored-by: JESSIELY JUACHON <jjuachon@vmware.com>

--- lib/sgtn-client/i18n_backend.rb
+++ lib/sgtn-client/i18n_backend.rb
@@ -4,8 +4,7 @@
 require 'i18n'
 
 module SgtnClient # :nodoc:
-  # Sometimes it is needed to use Singleton as an I18n backend to minimize the changes when integrating Singleton with
-  # your application. Here is the common usage:
+  # When integrating Singleton in a client application that is already using [I18n::Backend (https://www.rubydoc.info/github/svenfuchs/i18n/master/I18n/Backend/), it would be useful to have Singleton override the said module in order to minimize necessary changes. Here is a common usage:
   #
   #   I18n::Backend::Simple.include(I18n::Backend::Fallbacks) # add fallbacks behavior to current backend
   #   I18n.backend = I18n::Backend::Chain.new(Sgtn::I18nBackend.new(component_name), I18n.backend)
