837368a87e6d6aabf093ee73b42d6783d2874a02
xiaochao <48587632+Xiaochao8@users.noreply.github.com>
2022-09-14 06:24:54 +0000

    ccc

--- spec/unit/cache_spec.rb
+++ spec/unit/cache_spec.rb
@@ -172,10 +172,10 @@ describe 'Cache' do
   end
 
   it '#default cache expiration' do
-      Sgtn.cache_expiry_period = nil
-      SgtnClient::Common::DataInfo.instance_variable_set(:@age, nil)
-      expect(SgtnClient::Common::DataInfo.age).to eq 24 * 60 *60
-    ensure
-      SgtnClient::Common::DataInfo.instance_variable_set(:@age, nil)
-    end
+    Sgtn.cache_expiry_period = nil
+    SgtnClient::Common::DataInfo.instance_variable_set(:@age, nil)
+    expect(SgtnClient::Common::DataInfo.age).to eq 24 * 60 *60
+  ensure
+    SgtnClient::Common::DataInfo.instance_variable_set(:@age, nil)
+  end
 end
