2d5656f19bbe6712243daccfc29c2f02ce96d55e
xiaochao <48587632+Xiaochao8@users.noreply.github.com>
2022-09-21 11:20:04 +0000

    ccc

--- api/v2/translation/handler_trans.go
+++ api/v2/translation/handler_trans.go
@@ -275,7 +275,7 @@ func ConvertBundleToInternal(apiData *UpdateBundle) []*translation.Bundle {
 }
 
 func ConvertReleaseToAPI(release *translation.Release) *ReleaseData {
-	if release == nil && len(release.Bundles) == 0 {
+	if release == nil || len(release.Bundles) == 0 {
 		return nil
 	}
 
