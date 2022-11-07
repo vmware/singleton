85960e15aff52a0c560d505d55640adefe7002d2
xiaochao <48587632+Xiaochao8@users.noreply.github.com>
2022-11-07 01:56:12 +0000

    ccc

--- lib/sgtn-client/loader/pseudo.rb
+++ lib/sgtn-client/loader/pseudo.rb
@@ -4,35 +4,21 @@
 module SgtnClient
   module TranslationLoader
     module Pseudo # :nodoc:
-      def load_bundle(component, locale)
-        return super unless locale == Sgtn::PSEUDO_LOCALE
+      protected
 
-        SgtnClient.logger.debug { "[#{__FILE__}][#{__callee__}] component=#{component}, locale=#{locale}" }
+      def old_source_locale(locale)
+        locale == Sgtn::PSEUDO_LOCALE ? CONSTS::REAL_SOURCE_LOCALE : super
+      end
 
-        begin
-          old_source_bundle_thread = Thread.new { load_bundle(component, CONSTS::OLD_SOURCE_LOCALE) }
-          source_bundle = load_bundle(component, LocaleUtil.get_source_locale)
-          old_source_bundle = old_source_bundle_thread.value
-        rescue StandardError => e
-          SgtnClient.logger.error "[#{__FILE__}][#{__callee__}] failed to load source(or old source) bundle. component:#{component}. error: #{e}"
-          raise e
+      def compare_source(translation_bundle, old_source_bundle, source_bundle)
+        if translation_bundle.locale == Sgtn::PSEUDO_LOCALE
+          return translation_bundle if translation_bundle.origin == source_bundle.origin
+
+          tag = source_bundle.origin.pseudo_tag
+          super.transform_values! { |v| v.is_a?(LocalizedString) ? "#{tag}#{v}#{tag}" : v }
+        else
+          super
         end
-
-        translation_bundle = if old_source_bundle.origin == source_bundle.origin
-                               source_bundle
-                             else
-                               compare_source(old_source_bundle, old_source_bundle, source_bundle)
-                             end
-        tag = source_bundle.origin.pseudo_tag
-        oldtag = old_source_bundle.origin.pseudo_tag
-        translation_bundle.each do |key, value|
-          translation_bundle[key] = if value.is_a?(LocalizedString)
-                                      "#{tag}#{value}#{tag}"
-                                    else
-                                      "#{oldtag}#{value}#{oldtag}"
-                                    end
-        end
-        translation_bundle
       end
     end
   end
