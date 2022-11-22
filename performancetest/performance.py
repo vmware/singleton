a4ae937ff03e6189924c9778d424d4f2f54471d9
BeijingMa <beijingm@vmware.com>
2022-11-22 16:15:17 +0800

    update

--- performancetest/performance.py
+++ performancetest/performance.py
@@ -1,94 +1,65 @@
-"""
-PMeter     --  ThreadGroup - -Collection1 -- -Api1
-                                             -Api2
-                                             -Api3
-                           - -Collection1
-
-           --  ThreadGroup - -Collection2
-                             -Collection2
-
-PMeter Mainly responsible for starting the serial startup Tasks, all the Tasks are executed, and the data is analyzed. Display data.
-ThreadGroup Collection for multi-threaded concurrent testing. Thread_number and loop_count can be configured to control the concurrency scenarios of the collection. Thread group corresponding to jmete
-HttpCollection Used to add the interface under test. Multiple APIs can be added. Use requests.Session multiplexing to reduce network overhead
-
-Test scenario 1, 10 threads, loop 10 times, test 1 scenario 22api
-thread_group = ThreadGroup()
-thread_group.create_task(thread_number=10, loop_count=2, HttpCollection())
-thread_group.run()
-
-Test scenario 2, 1 thread, duration 60 seconds
-thread_group = ThreadGroup()
-thread_group.create_task(thread_number=2, duration=60, HttpCollection(1))
-thread_group.create_task(thread_number=2, duration=10, HttpCollection(2))
-thread_group.run()
-
-"""
 import time
 import uuid
 import threading
 from queue import Queue
+from pathlib import Path
 from typing import Optional
 
+import yaml
 import requests
-from requests.exceptions import RequestException
 from loguru import logger
 
 from utils import read_json, TestCase, Parameters
 
+_CONFIG_ = Path(__file__).parent.joinpath("config.yaml")
+
 BASE_URL: str = "https://localhost:8090"
+
+config: dict = yaml.safe_load(_CONFIG_.read_bytes())
 
 
 class CollectionResponse:
-    __slots__ = ["case_name", "success", "response_time", "response_content"]
+    __slots__ = ["case_name", "success", "status", "response_time", "response_content"]
 
     def __init__(self):
         self.case_name: str = ''
         self.success: bool = False
+        self.status: int = 0  # 0：success， 1：success(warning)，2：fail，3：error
         self.response_time: float = 1000000.0
         self.response_content: dict = {}
 
 
 class HttpCollection:
-    """
-    1. To achieve support for multiple APIs, loop execution, and provide a unified scheduling entry __call__()
-    2，read from json file
-    """
-
-    def __init__(self, name: str, file: str):
+    def __init__(self, name: str, file: str, threshold_limit: float):
         self.id: str = str(uuid.uuid4())
         self.name: str = name
         self.http_session = requests.Session()
         self.testcases: list[TestCase] = read_json(file)
         self.total_time: float = 0
+        self.threshold_limit: float = threshold_limit
+        self.success: bool = True
 
-    def validate(self, response: requests.Response, case: TestCase):
+    def get_case_by_name(self, name: str) -> TestCase:
+        for case in self.testcases:
+            if case.name == name:
+                return case
+
+    @staticmethod
+    def validate(response: requests.Response, case: TestCase):
         # assert http status_code == 200
-        assert response.status_code == 200, f"Bad Http Request!, status_code: {response.status_code} != 200."
+        assert response.status_code == 200, f"Bad Request! status_code: {response.status_code} != 200."
 
         # assert response return_code
         code: int = response.json().get("response", {}).get("code", -1)
-        validate_code: int = case.validators.get("return_code", -100)
-        error_msg2 = (f'Actual return_code: {code} '
-                      f'Expected return_code: {validate_code} are inconsistent.'
-                      f'The Actual Response: {response.json()}')
-        assert code == validate_code, error_msg2
+        expected_code: int = case.validators.get("return_code", -100)
+        assert code == expected_code, f"Bad Response! return_code: {code} is not expected({expected_code})."
 
-        # assert response_time
-        expected_validate_time: float = case.validators.get("response_time", 0)
-        response_time: float = round(response.elapsed.total_seconds() * 1000, 3)
-        error_msg3 = (f'Actual response time: {"%.3f" % response_time}ms '
-                      f'Expected response time: {expected_validate_time}ms')
-        assert response_time < expected_validate_time, error_msg3
-
-        # assert response_content
-
-    def execute(self, case: TestCase, q: Queue, p: Parameters) -> None:
-        thread_id: str = threading.current_thread().name
+    def execute(self, case: TestCase, q: Queue, p: Parameters):
         resp: CollectionResponse = CollectionResponse()
         resp.case_name = case.name
 
         _product_name: str = p.get_ele()
-        _url = case.url.format(productName=_product_name)
+        url: str = BASE_URL + case.url.format(productName=_product_name)
         if case.params.get("productName", None):
             case.params["productName"] = _product_name
 
@@ -96,44 +67,42 @@ class HttpCollection:
             if case.body.get("data", {}).get("productName", None):
                 case.body["data"]["productName"] = _product_name
 
-        url: str = BASE_URL + _url
         try:
-            execute_start: float = time.time() * 1000
             r: requests.Response = self.http_session.request(case.method, url, params=case.params, json=case.body,
                                                              headers=case.headers, verify=False)
-            duration_time: float = round(time.time() * 1000 - execute_start, 3)
-        except RequestException as e:
-            # http request error
-            logger.error((f'[{thread_id}] TestCase: <{case.name}> execute failed.\n'
-                          f'{"*" * 30} request_data {"*" * 30}\n'
-                          f'url= {url}\n'
-                          f'json= {case.body}\n'
-                          f"response={{}}\n"
-                          f'error_msg= {e}\n'
-                          f'{"*" * 74}\n'))
+        except Exception as e:
+            resp.status = 3  # http request error or code error
+            logger.critical((f'🆔{case.name}💣ERROR\n'
+                             f'{"*" * 30} request_data {"*" * 30}\n'
+                             f'url= {url}\n'
+                             f'json= {case.body}\n'
+                             f"response={{}}\n"
+                             f'error_msg= {e}\n'
+                             f'{"*" * 74}\n'))
 
         else:
             # http request ok and validate
             try:
                 self.validate(r, case)
             except AssertionError as e:
+                resp.status = 2  # fail
                 resp.response_time = round(r.elapsed.total_seconds() * 1000, 3)
                 resp.response_content = r.json()
-                logger.error((f'[{thread_id}] TestCase: <{case.name}> execute failed.\n'
+                logger.error((f'🆔{case.name}❌FAIL\n'
                               f'{"*" * 30} request_data {"*" * 30}\n'
                               f'url= {r.request.url}\n'
                               f'json= {case.body}\n'
-                              f"response={r.json()}\n"
+                              f"response={str(r.json())[0:200]}...\n"
                               f'error_msg= {e}\n'
                               f'{"*" * 74}\n'))
 
             except Exception as e:
-                # default, code error
-                logger.critical((f'[{thread_id}] TestCase: <{case.name}> execute exception!!!\n'
+                resp.status = 3  # code error
+                logger.critical((f'🆔{case.name}💣ERROR\n'
                                  f'{"*" * 30} request_data {"*" * 30}\n'
                                  f'url= {r.request.url}\n'
                                  f'json= {case.body}\n'
-                                 f"response={r.json()}\n"
+                                 f"response={r.text[0:200]}...\n"
                                  f'error_msg= {e}\n'
                                  f'{"*" * 74}\n'))
 
@@ -141,17 +110,29 @@ class HttpCollection:
                 resp.response_time = round(r.elapsed.total_seconds() * 1000, 3)
                 resp.response_content = r.json()
                 resp.success = True
-                logger.debug(
-                    f'[{thread_id}] TestCase: <{case.name}> execute success! duration:{duration_time}ms!')
+
+                response_time_threshold: float = case.validators.get("response_time_threshold", 0)
+                if resp.response_time > response_time_threshold:
+                    resp.status = 1
+                    logger.warning(f'🆔{case.name}💢WARN🕜:{"%.3f" % resp.response_time}ms!')
+                else:
+                    resp.status = 0
+                    logger.debug(f'🆔{case.name}✅PASS🕜:{"%.3f" % resp.response_time}ms!')
+
         q.put(resp)
 
-    def __call__(self, index: int, q: Queue, loop_count: Optional[int], duration: Optional[float]):
+    def __call__(self, index: int, q: Queue, loop_count: Optional[int] = None, duration: Optional[float] = None):
         """
         There are durations, which are executed through durations. Cycle times fail
         Otherwise, execute the number of loops
         """
         p = Parameters(index)
-        if duration:
+
+        if loop_count:
+            for case in self.testcases:
+                for i in range(loop_count):
+                    self.execute(case, q, p)
+        elif duration:
             start_tsp: float = time.time()
             current_tsp: float = time.time()
 
@@ -163,11 +144,6 @@ class HttpCollection:
                         self.execute(case, q, p)
                     else:
                         break
-
-        else:
-            for case in self.testcases:
-                for i in range(loop_count):
-                    self.execute(case, q, p)
 
 
 class ThreadGroup:
@@ -215,13 +191,15 @@ class PMeter:
         self.q_map: dict[HttpCollection, Queue] = {}  # the map between http_collection and queue
 
         self.collections_data: dict[HttpCollection, list[CollectionResponse]] = {}
-        self.collections_map: dict[HttpCollection, dict[str, list[CollectionResponse]]] = {}
-        self.collections_result: dict[HttpCollection, bool] = {}
+        self.collections_map: dict[
+            HttpCollection, dict[str, list[CollectionResponse]]] = {}
+
+        self.collections_result_map: dict[HttpCollection, dict[str, dict]] = {}
 
     def create_task(self, collection: HttpCollection, thread_group_name: str = None,
                     thread_number: int = 1, loop_count: Optional[int] = 1,
                     duration: Optional[float] = None) -> 'PMeter':
-        q = Queue()
+        q: Queue = Queue()
         target: ThreadGroup = ThreadGroup(thread_number=thread_number, q=q,
                                           loop_count=loop_count, duration=duration).create(collection)
         self.task_group.append(threading.Thread(target=target, name=thread_group_name))
@@ -249,59 +227,95 @@ class PMeter:
                 _collection.setdefault(_resp.case_name, []).append(_resp)
             self.collections_map[collection] = _collection
 
-        for collection, q_list in self.collections_data.items():
-            self.collections_result[collection] = all([_data.success for _data in q_list])
+        for collection, case_data in self.collections_map.items():
+            _analysis: dict[str, dict] = {}
+
+            threshold_limit: float = collection.threshold_limit
+            for case_name, response_list in case_data.items():
+                result: bool = True
+                total_num: int = len(response_list)
+                success_num: int = len([response for response in response_list if response.success])
+                warn_num: int = len([response for response in response_list if response.status == 1])
+                fail_num: int = len([response for response in response_list if response.status == 2])
+                error_num: int = len([response for response in response_list if response.status == 3])
+                success_responses: list[float] = [r.response_time for r in response_list if r.success]
+                avg: float = round(sum(success_responses) / success_num, 2) if success_num else float(0)
+                pass_rate: float = (1 - warn_num / success_num) if success_num else float(0)
+
+                tc: TestCase = collection.get_case_by_name(case_name)
+                average_response_time: float = tc.validators.get("average_response_time", float(0))
+
+                if fail_num or error_num:
+                    result = False
+                if avg > average_response_time:
+                    result = False
+                if pass_rate < threshold_limit:
+                    result = False
+                analysis_data: dict = {"Total": total_num, "Success": success_num, "Warn": warn_num, "Fail": fail_num,
+                                       "Error": error_num,
+                                       "AverageTime": avg, "PassRate": pass_rate, "threshold_limit": threshold_limit,
+                                       "average_response_time": average_response_time, "result": result}
+                _analysis[case_name] = analysis_data
+                collection.success = collection.success and result
+            self.collections_result_map[collection] = _analysis
 
         return self
 
-    def exit(self) -> None:
-        result: bool = True
-        for _result in self.collections_result.values():
-            _result: bool
-            result = result and _result
-        exit(-1) if not result else None
+    def exit(self):
+        exit_code = -1
+        for collection in self.collections_result_map.keys():
+            if not collection.success:
+                exit(exit_code)
 
     def analysis(self):
-        for collection, data in self.collections_map.items():
-            logger.info("@" + f' Analysis <{collection.name}> '.center(162, '@') + "@")
-            self.average(data)
+        for collection, data in self.collections_result_map.items():
+            logger.info("@" + f' Analysis <{collection.name}> '.center(168, '@') + "@")
+            self.average(collection, data)
 
-    def average(self, collection_data: dict[str, list[CollectionResponse]]):
-        logger.info("|" + f"Average Response Time Table".center(163, '-') + "|")
-        logger.info("|" + f"response_time".center(13, '-') +
-                    "|" + f"total_count".center(11, '-') +
-                    "|" + f"error_count".center(11, '-') +
-                    "|" + f"testcase".center(125, '-') + "|")
-        for case_name, response_list in collection_data.items():
-            total_num: int = len(response_list)
-            success_num: int = len([response for response in response_list if response.success])
-            success_responses: list[float] = [r.response_time for r in response_list if r.success]
-            avg: float = round(sum(success_responses) / success_num, 3) if success_num else 0
+    @staticmethod
+    def average(collection: HttpCollection, collection_data: dict[str, dict]):
+        logger.info("|" + f"Average Response Time Table".center(169, '-') + "|")
+        logger.info("|" + f"➡".center(1, '-') +
+                    "|" + f"Total".center(5, '-') +
+                    "|" + f"Warn".center(4, '-') +
+                    "|" + f"Fail".center(4, '-') +
+                    "|" + f"Error".center(5, '-') +
+                    "|" + f"AverageTime".center(11, '-') +
+                    "|" + f"Threshold".center(9, '-') +
+                    "|" + f"Testcase".center(122, '-') + "|")
+        for case_name, response_dict in collection_data.items():
+            success: str = "✔" if response_dict["result"] else "❌"
+            total_num: int = response_dict["Total"]
+            warn_num: int = response_dict["Warn"]
+            fail_num: int = response_dict["Fail"]
+            error_num: int = response_dict["Error"]
+            avg: float = response_dict["AverageTime"]
+            pass_rate: str = f'{round(response_dict["PassRate"] * 100, 2)}%'
 
-            logger.info("|" + f"{avg}ms".ljust(13) +
-                        "|" + f"{total_num}".ljust(11) +
-                        "|" + f"{total_num - success_num}".ljust(11) +
-                        "|" + f"{case_name}".ljust(125) + "|")
-        logger.info("|" + f"-".center(163, '-') + "|")
+            logger.info("|" + f"{success}".ljust(1) +
+                        "|" + f"{total_num}".ljust(5) +
+                        "|" + f"{warn_num}".ljust(4) +
+                        "|" + f"{fail_num}".ljust(4) +
+                        "|" + f"{error_num}".ljust(5) +
+                        "|" + f"{avg}ms".ljust(11) +
+                        "|" + f"{pass_rate}".ljust(9) +
+                        "|" + f"{case_name}".ljust(122) + "|")
+        logger.info("|" + f"-".center(169, '-') + "|")
 
 
 if __name__ == '__main__':
-    import yaml
-    from pathlib import Path
 
-    _CONFIG_ = Path(__file__).parent.joinpath("config.yaml")
-
-    config: dict = yaml.safe_load(_CONFIG_.read_bytes())
     tsp: float = time.time() * 1000
     pmeter = PMeter()
 
     for conf in config.get("HttpCollections", []):
-        pmeter.create_task(collection=HttpCollection(name=conf["name"], file=conf["file"]),
-                           thread_number=conf["thread_number"], loop_count=conf["loop_count"],
-                           thread_group_name=conf["thread_group_name"])
+        pmeter.create_task(
+            collection=HttpCollection(name=conf["name"], file=conf["file"], threshold_limit=conf["threshold_limit"]),
+            thread_number=conf["thread_number"], loop_count=conf["loop_count"],
+            thread_group_name=conf["thread_group_name"])
 
     pmeter.run()
     pmeter.analysis()
     cost: float = round(time.time() * 1000 - tsp, 3)
-    logger.info(f"Test Completed in {cost}ms!")
+    logger.info(f"Test Completed in 🕑{cost}ms!")
     pmeter.exit()
