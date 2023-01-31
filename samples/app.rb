2afc2cff0c4b5c07b874387232be7b7990629211
Xiaochao Li <48587632+Xiaochao8@users.noreply.github.com>
2023-01-31 11:36:15 +0800

    remove version limit on dependent libs and update sample (#2063)

--- samples/app.rb
+++ samples/app.rb
@@ -1,12 +1,11 @@
-# Copyright 2022 VMware, Inc.
+# Copyright 2022-2023 VMware, Inc.
 # SPDX-License-Identifier: EPL-2.0
 
 # Configure Bundler
 require 'bundler/setup'
-require './runner.rb'
+require './runner'
 
 Bundler.require :default, :sample
-
 
 class App < Sinatra::Application
   enable :sessions
@@ -15,26 +14,25 @@ class App < Sinatra::Application
     haml :index
   end
 
-  get "/translation/all" do
-    @Result = RunSample.run("translation/all.rb", "@Result")
-    haml :display_hash, :locals => {
-                          :header => "translations",
-                          :display_hash => @Result
-                        }
+  get '/translation/all' do
+    @Result = RunSample.run('translation/all.rb', '@result')
+    haml :display_hash, locals: {
+      header: 'translations',
+      display_hash: @Result
+    }
   end
 
-  get "/formatting/all" do
-    @Result  = RunSample.run("formatting/all.rb", "@Result")
-    haml :display_hash, :locals => {
-      :header => "Got 1 matching payments",
-      :display_hash => {"success": true, "result": @Result} }
+  get '/formatting/all' do
+    @Result = RunSample.run('formatting/all.rb', '@result')
+    haml :display_hash, locals: {
+      header: 'Got 1 matching payments',
+      display_hash: { "success": true, "result": @Result }
+    }
   end
 
-
-  Dir["translation/*", "formatting/*"].each do |file_name|
-    get "/#{file_name.sub(/rb$/, "html")}" do
-      CodeRay.scan(File.read(file_name), "ruby").page :title => "Source: #{file_name}"
+  Dir['translation/*', 'formatting/*'].each do |file_name|
+    get "/#{file_name.sub(/rb$/, 'html')}" do
+      CodeRay.scan(File.read(file_name), 'ruby').page title: "Source: #{file_name}"
     end
   end
-
 end
--- samples/translation/all.rb
+++ samples/translation/all.rb
@@ -1,11 +1,14 @@
-# Copyright 2022 VMware, Inc.
+# Copyright 2022-2023 VMware, Inc.
 # SPDX-License-Identifier: EPL-2.0
 
 require 'singleton-client'
 
 # load config file to initialize app
 Sgtn.load_config('./config/sgtnclient.yml', 'test')
+Sgtn.pseudo_tag = '@&'
 
+key = 'helloworld'
+plural_key = 'plural_key'
 component = 'JAVA'
 locale = 'zh-CN'
 en_locale = 'en'
@@ -18,50 +21,53 @@ setting_locale = {}
 string_translation = { 'normal' => normal_cases, 'plural' => plural_cases, 'formatting' => formatting_cases, 'setting locale' => setting_locale }
 
 # translate a string
-normal_cases['translate a string'] = Sgtn.t('helloworld', component, locale)
+normal_cases['translate a string'] = Sgtn.t!(key, component, locale)
 
 # translate an English string
-normal_cases['translate an English string'] = Sgtn.t('helloworld', component, en_locale)
+normal_cases['translate an English string'] = Sgtn.t!(key, component, en_locale)
 
 # translate a string only in source
-normal_cases['translate a string only in source'] = Sgtn.t('hello', component, locale)
+normal_cases['translate a string only in source'] = Sgtn.t!('hello', component, locale)
 
 # translate a string whose source is changed
-normal_cases['translate a string whose source is changed'] = Sgtn.t('source_changed_key', component, locale, place_holder: 'sky')
+normal_cases['translate a string whose source is changed'] = Sgtn.t!('source_changed_key', component, locale, place_holder: 'sky')
 
 # translate a non-existing string
-normal_cases['translate a nonexistent string returning key'] = Sgtn.t('nonexistent', component, locale)
-normal_cases['translate a nonexistent string with default value'] = Sgtn.t('nonexistent', component, locale) { 'default value when translation is missing' }
+normal_cases['translate a nonexistent string, returning key'] = Sgtn.t('nonexistent', component, locale)
+
+# pseudo translation
+string_translation['pseudo translation'] = Sgtn.t!(key, component, Sgtn::PSEUDO_LOCALE)
 
 # translate a string with pluralization
-plural_cases['translate a plural string - 0'] = Sgtn.t('plural_key', component, locale, cat_count: 0, place: '房间')
-plural_cases['translate a plural string - 1'] = Sgtn.t('plural_key', component, locale, cat_count: 1, place: '盒子')
-plural_cases['translate a plural string - 2'] = Sgtn.t('plural_key', component, locale, cat_count: 2, place: 'bush')
+plural_cases['translate a plural string - 0'] = Sgtn.t!(plural_key, component, locale, cat_count: 0, place: '房间')
+plural_cases['translate a plural string - 1'] = Sgtn.t!(plural_key, component, locale, cat_count: 1, place: '盒子')
+plural_cases['translate a plural string - 2'] = Sgtn.t!(plural_key, component, locale, cat_count: 2, place: 'bush')
 plural_cases['-----------------------'] = ''
-plural_cases['translate an English plural string - 0'] = Sgtn.t('plural_key', component, en_locale, cat_count: 0, place: 'room')
-plural_cases['translate an English plural string - 1'] = Sgtn.t('plural_key', component, en_locale, cat_count: 1, place: 'box')
-plural_cases['translate an English plural string - 2'] = Sgtn.t('plural_key', component, en_locale, cat_count: 2, place: 'bush')
+plural_cases['translate an English plural string - 0'] = Sgtn.t!(plural_key, component, en_locale, cat_count: 0, place: 'room')
+plural_cases['translate an English plural string - 1'] = Sgtn.t!(plural_key, component, en_locale, cat_count: 1, place: 'box')
+plural_cases['translate an English plural string - 2'] = Sgtn.t!(plural_key, component, en_locale, cat_count: 2, place: 'bush')
 
 # format translation
-formatting_cases["translate a string with placeholders - #{locale}"] = Sgtn.t('welcome', component, locale, place: '虚拟世界', name: '机器人')
-formatting_cases["translate a string with placeholders - #{en_locale}"] = Sgtn.t('welcome', component, en_locale, place: '虚拟世界', name: '机器人')
+formatting_cases["translate a string with placeholders - #{locale}"] = Sgtn.t!('welcome', component, locale, place: '虚拟世界', name: '机器人')
+formatting_cases["translate a string with placeholders - #{en_locale}"] = Sgtn.t!('welcome', component, en_locale, place: '虚拟世界', name: '机器人')
 
 # set locale
 Sgtn.locale = locale
-setting_locale["set loale to #{locale} before translating"] = Sgtn.t('helloworld', component)
+setting_locale["set locale to #{locale} before translating"] = Sgtn.t!(key, component)
 Sgtn.locale = en_locale
-setting_locale["set loale to #{en_locale} before translating"] = Sgtn.t('helloworld', component)
+setting_locale["set locale to #{en_locale} before translating"] = Sgtn.t!(key, component)
 ##########################################################
 
 ################## bundle translations ###################
 bundle_translations = {}
-bundle_translations["#{locale} translations"] = Sgtn.get_translations(component, locale)
-bundle_translations['English translations'] = Sgtn.get_translations(component, en_locale)
+bundle_translations["#{locale} translations"] = Sgtn.get_translations!(component, locale)
+bundle_translations['English translations'] = Sgtn.get_translations!(component, en_locale)
 Sgtn.locale = locale
-bundle_translations["set loale to #{locale}"] = Sgtn.get_translations(component)
+bundle_translations["set locale to #{locale}"] = Sgtn.get_translations!(component)
+bundle_translations['pseudo translations'] = Sgtn.get_translations!(component, Sgtn::PSEUDO_LOCALE)
+
 ##########################################################
 
+@result = { 'string translation' => string_translation, 'bundle translations' => bundle_translations }
 
-@Result = { 'string translation' => string_translation, 'bundle translations' => bundle_translations }
-
-pp @Result
+puts JSON.pretty_generate(@result)
