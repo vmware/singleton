2e38e083452c60ecdb0d2062487550fa3c7406a1
yiyu <lyiyu66@hotmail.com>
2023-02-01 15:30:20 +0800

    update the root path of zip files

--- autotest/cldrpattern/constant.py
+++ autotest/cldrpattern/constant.py
@@ -1,5 +1,5 @@
 CaseFile = 'ExtractedPatternLibTestCases.xlsx' #The testcase excle
-rootdir = '../src/main/resources/cldr/data' #The path of cldr data storage
+rootdir = './src/main/resources/cldr/data' #The path of cldr data storage
 test_dir = './' #The directory of generated test report
 version = '/32.0.0' # cldr version
 unzipfile_dir = './unzipfile/' # the dirctory of storing extracted files
