2ea6cf05a9887d8da9b489d440144ea2ed30f321
Shi Hu <shihu@vmware.com>
2023-02-02 10:29:38 +0800

    fixed the comments

--- g11n-ws/modules/md-restful-api-i18n/src/main/java/com/vmware/vip/i18n/api/base/StreamProductAction.java
+++ g11n-ws/modules/md-restful-api-i18n/src/main/java/com/vmware/vip/i18n/api/base/StreamProductAction.java
@@ -4,6 +4,18 @@
  */
 package com.vmware.vip.i18n.api.base;
 
+import com.vmware.vip.common.constants.ConstantsChar;
+import com.vmware.vip.common.constants.ConstantsKeys;
+import com.vmware.vip.common.constants.ConstantsMsg;
+import com.vmware.vip.common.i18n.status.APIResponseStatus;
+import com.vmware.vip.core.messages.exception.L3APIException;
+import com.vmware.vip.core.messages.service.multcomponent.IMultComponentService;
+import com.vmware.vip.core.messages.service.product.IProductService;
+import com.vmware.vip.messages.data.dao.model.ResultMessageChannel;
+import org.apache.commons.lang3.StringUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.nio.ByteBuffer;
 import java.nio.channels.Channels;
@@ -13,162 +25,150 @@ import java.nio.channels.WritableByteChannel;
 import java.util.ArrayList;
 import java.util.List;
 
-import javax.servlet.http.HttpServletResponse;
+public class StreamProductAction extends TranslationProductAction {
+    private static byte[] byteComm = (ConstantsChar.COMMA + "\r\n").getBytes();
 
-import org.apache.commons.lang3.StringUtils;
-import org.springframework.beans.factory.annotation.Autowired;
+    @Autowired
+    IProductService productService;
 
-import com.vmware.vip.common.constants.ConstantsChar;
-import com.vmware.vip.common.constants.ConstantsKeys;
-import com.vmware.vip.common.constants.ConstantsMsg;
-import com.vmware.vip.common.i18n.status.APIResponseStatus;
-import com.vmware.vip.core.messages.exception.L3APIException;
-import com.vmware.vip.core.messages.service.multcomponent.IMultComponentService;
-import com.vmware.vip.core.messages.service.product.IProductService;
-import com.vmware.vip.messages.data.dao.model.ResultMessageChannel;
+    @Autowired
+    IMultComponentService multComponentService;
 
-public class StreamProductAction extends TranslationProductAction{
-	 private static byte[] byteComm = (ConstantsChar.COMMA+"\r\n").getBytes();
-	
-	@Autowired
-	IProductService productService;
-	
-	@Autowired
-	IMultComponentService multComponentService;
-	
-	
-	public void writeMultTranslationResponse(String productName, String version, String components, String locales,
-			String pseudo, HttpServletResponse resp) throws Exception {
 
-		String newVersion = super.getAvailableVersion(productName, version);
-		List<String> componentList = getcomponentList(productName, newVersion, components);
-		List<String> localeList = getlocaleList(productName, newVersion, locales, pseudo);
+    public void writeMultTranslationResponse(String productName, String version, String components, String locales,
+                                             String pseudo, HttpServletResponse resp) throws Exception {
 
-		List<ResultMessageChannel>  readChannels = multComponentService.getTranslationChannels(productName, newVersion, componentList, localeList);
-		if(readChannels.isEmpty()) {
+        String newVersion = super.getAvailableVersion(productName, version);
+        List<String> componentList = getcomponentList(productName, newVersion, components);
+        List<String> localeList = getlocaleList(productName, newVersion, locales, pseudo);
+
+        List<ResultMessageChannel> readChannels = multComponentService.getTranslationChannels(productName, newVersion, componentList, localeList);
+        if (readChannels.isEmpty()) {
             throw new L3APIException(ConstantsMsg.TRANS_IS_NOT_FOUND);
         }
 
-		StreamProductResp sr = new StreamProductResp(productName, newVersion, localeList, componentList, pseudo, false);
-		resp.setContentType(ConstantsKeys.CONTENT_TYPE_JSON);
-		WritableByteChannel wbc = Channels.newChannel(resp.getOutputStream());
+        StreamProductResp sr = new StreamProductResp(productName, newVersion, localeList, componentList, pseudo, false);
+        resp.setContentType(ConstantsKeys.CONTENT_TYPE_JSON);
+        WritableByteChannel wbc = Channels.newChannel(resp.getOutputStream());
 
-		boolean isPartContent = writeResponseHeader(version.equals(newVersion), componentList.size() * localeList.size(), readChannels.size(), wbc, sr);
-		writeTranslationsToChanel(readChannels, wbc);
-		 if(isPartContent){
-			 wbc.write(ByteBuffer.wrap(addNullMessage(readChannels, componentList, localeList)));
-		 }
-		 wbc.write(sr.getEndBytes());
-	}
+        boolean isPartContent = writeResponseHeader(version.equals(newVersion), componentList.size() * localeList.size(), readChannels.size(), wbc, sr);
+        writeTranslationsToChannel(readChannels, wbc);
+        if (isPartContent) {
+            wbc.write(ByteBuffer.wrap(addNullMessage(readChannels, componentList, localeList)));
+        }
+        wbc.write(sr.getEndBytes());
+    }
 
-	private boolean writeResponseHeader(boolean notVersionFallback, int expSize, int readChannelSize, WritableByteChannel wbc, StreamProductResp sr) throws IOException {
-		boolean isPartContent = false;
-		if(notVersionFallback) {
-			isPartContent = (readChannelSize != expSize);
-			if(isPartContent) {
-				wbc.write(sr.getRespStartBytes(APIResponseStatus.MULTTRANSLATION_PART_CONTENT.getCode()));
-			}else {
-				wbc.write(sr.getRespStartBytes(APIResponseStatus.OK.getCode()));
-			}
-		}else {
-			wbc.write(sr.getRespStartBytes(APIResponseStatus.VERSION_FALLBACK_TRANSLATION.getCode()));
-		}
-		return isPartContent;
-	}
-	
-	
-	private List<String> getcomponentList(String productName, String versionStr, String components) throws L3APIException{
-		List<String> componentList = null;
-		if (StringUtils.isEmpty(components)) {
-			componentList = productService.getComponentNameList(productName, versionStr);
-		} else {
-			componentList = new ArrayList<String>();
-			for (String component : components.split(",")) {
-				componentList.add(component.trim());
-			}
-		}
-		
-		return componentList;
-	}
-	
-	private List<String> getlocaleList(String productName, String versionStr, String locales, String pseudo) throws L3APIException{
-		List<String> localeList = new ArrayList<String>();
-		if (Boolean.valueOf(pseudo)) {
-			localeList.add(ConstantsKeys.LATEST);
-		} else if (!StringUtils.isEmpty(locales)) {
-			for (String locale : locales.split(",")) {
-				localeList.add(super.getMappingLocale(productName, versionStr, locale.trim()));
-			}
-		} else {
-			localeList = productService.getSupportedLocaleList(productName, versionStr);
-		}
-		
-		return localeList;
-	}
-	
-	private byte[] addNullMessage(List<ResultMessageChannel>  readChannels, List<String> components, List<String> locales) {
-		StringBuilder sb = new StringBuilder();
-		for (String component : components) {
-			for (String locale : locales) {
-				ResultMessageChannel rmc = new ResultMessageChannel(component, locale, null);
-				if(!readChannels.contains(rmc)) {
-					sb.append(",");
-					sb.append(rmc.generateNullMessage());
-				}
-				
-			}
-		}
-		return sb.toString().getBytes();
-	}
-  
+	/**
+	 * write the response header bytes to channel
+	 */
+    private boolean writeResponseHeader(boolean notVersionFallback, int expSize, int readChannelSize, WritableByteChannel wbc, StreamProductResp sr) throws IOException {
+        boolean isPartContent = false;
+        if (notVersionFallback) {
+            isPartContent = (readChannelSize != expSize);
+            if (isPartContent) {
+                wbc.write(sr.getRespStartBytes(APIResponseStatus.MULTTRANSLATION_PART_CONTENT.getCode()));
+            } else {
+                wbc.write(sr.getRespStartBytes(APIResponseStatus.OK.getCode()));
+            }
+        } else {
+            wbc.write(sr.getRespStartBytes(APIResponseStatus.VERSION_FALLBACK_TRANSLATION.getCode()));
+        }
+        return isPartContent;
+    }
 
 
-	private void transferTo(ReadableByteChannel readChannel, WritableByteChannel writeChannel, ByteBuffer buf) throws Exception {
+    private List<String> getcomponentList(String productName, String versionStr, String components) throws L3APIException {
+        List<String> componentList = null;
+        if (StringUtils.isEmpty(components)) {
+            componentList = productService.getComponentNameList(productName, versionStr);
+        } else {
+            componentList = new ArrayList<String>();
+            for (String component : components.split(",")) {
+                componentList.add(component.trim());
+            }
+        }
 
-		if (readChannel instanceof FileChannel) {
-			try (FileChannel fileChannel = (FileChannel) readChannel) {
-				fileChannel.transferTo(0, fileChannel.size(), writeChannel);
-			}
-		} else {
-			
-			try {
-				while ((readChannel.read(buf)) != -1) {
-					buf.flip();
-					do {
-						writeChannel.write(buf);
-					}while (buf.hasRemaining());
-					
-					buf.clear();
-				}
-			} catch (Exception e) {
-				throw e;
-			} finally {
-				readChannel.close();
-			}
-		}
-	}
+        return componentList;
+    }
 
-	private void writeTranslationsToChanel(List<ResultMessageChannel>  readChannels, WritableByteChannel wbc) throws Exception {
-		ByteBuffer buf = null;
-		ReadableByteChannel firstReadChannel =  readChannels.get(0).getReadableByteChannel();
-		boolean flag = (firstReadChannel instanceof FileChannel);
-		if(flag) {
-			transferTo(readChannels.get(0).getReadableByteChannel(), wbc, null);
-			buf = ByteBuffer.wrap(byteComm);
-		}else {
-			buf = ByteBuffer.allocateDirect(16384);
-			transferTo(readChannels.get(0).getReadableByteChannel(), wbc, buf);
-		}
+    private List<String> getlocaleList(String productName, String versionStr, String locales, String pseudo) throws L3APIException {
+        List<String> localeList = new ArrayList<String>();
+        if (Boolean.valueOf(pseudo)) {
+            localeList.add(ConstantsKeys.LATEST);
+        } else if (!StringUtils.isEmpty(locales)) {
+            for (String locale : locales.split(",")) {
+                localeList.add(super.getMappingLocale(productName, versionStr, locale.trim()));
+            }
+        } else {
+            localeList = productService.getSupportedLocaleList(productName, versionStr);
+        }
 
-		for (int idx =1;  idx <readChannels.size(); idx++ ) {
-			if(flag) {
-				buf.rewind();
-				wbc.write(buf);
-			}else {
-				buf.put(byteComm);
-			}
-			transferTo(readChannels.get(idx).getReadableByteChannel(), wbc, buf);
-		}
-	}
+        return localeList;
+    }
+
+    private byte[] addNullMessage(List<ResultMessageChannel> readChannels, List<String> components, List<String> locales) {
+        StringBuilder sb = new StringBuilder();
+        for (String component : components) {
+            for (String locale : locales) {
+                ResultMessageChannel rmc = new ResultMessageChannel(component, locale, null);
+                if (!readChannels.contains(rmc)) {
+                    sb.append(",");
+                    sb.append(rmc.generateNullMessage());
+                }
+
+            }
+        }
+        return sb.toString().getBytes();
+    }
+
+
+    private void transferTo(ReadableByteChannel readChannel, WritableByteChannel writeChannel, ByteBuffer buf) throws Exception {
+
+        if (readChannel instanceof FileChannel) {
+            try (FileChannel fileChannel = (FileChannel) readChannel) {
+                fileChannel.transferTo(0, fileChannel.size(), writeChannel);
+            }
+        } else {
+
+            try {
+                while ((readChannel.read(buf)) != -1) {
+                    buf.flip();
+                    do {
+                        writeChannel.write(buf);
+                    } while (buf.hasRemaining());
+
+                    buf.clear();
+                }
+            } catch (Exception e) {
+                throw e;
+            } finally {
+                readChannel.close();
+            }
+        }
+    }
+
+    private void writeTranslationsToChannel(List<ResultMessageChannel> readChannels, WritableByteChannel wbc) throws Exception {
+        ByteBuffer buf = null;
+        ReadableByteChannel firstReadChannel = readChannels.get(0).getReadableByteChannel();
+        boolean flag = (firstReadChannel instanceof FileChannel);
+        if (flag) {
+            transferTo(readChannels.get(0).getReadableByteChannel(), wbc, null);
+            buf = ByteBuffer.wrap(byteComm);
+        } else {
+            buf = ByteBuffer.allocateDirect(16384);
+            transferTo(readChannels.get(0).getReadableByteChannel(), wbc, buf);
+        }
+
+        for (int idx = 1; idx < readChannels.size(); idx++) {
+            if (flag) {
+                buf.rewind();
+                wbc.write(buf);
+            } else {
+                buf.put(byteComm);
+            }
+            transferTo(readChannels.get(idx).getReadableByteChannel(), wbc, buf);
+        }
+    }
 
 }
--- g11n-ws/modules/md-restful-api-i18n/src/main/java/com/vmware/vip/i18n/api/base/StreamProductResp.java
+++ g11n-ws/modules/md-restful-api-i18n/src/main/java/com/vmware/vip/i18n/api/base/StreamProductResp.java
@@ -9,105 +9,104 @@ import java.util.List;
 
 public class StreamProductResp {
     private static String fallbackStateStr = "{\r\n  \"response\": {\r\n    \"code\": 604,\r\n    \"message\": \"The content of response have been version fallback\",\r\n    \"serverTime\": \"\"\r\n  },\r\n  \"signature\": \"\",\r\n  \"data\": {\r\n";
-    
-	private static String partSuccStateStr = "{\r\n  \"response\": {\r\n    \"code\": 207,\r\n    \"message\": \"Part of the translation is available\",\r\n    \"serverTime\": \"\"\r\n  },\r\n  \"signature\": \"\",\r\n  \"data\": {\r\n";
-	
-	private static String succStateStr = "{\r\n  \"response\": {\r\n    \"code\": 200,\r\n    \"message\": \"OK\",\r\n    \"serverTime\": \"\"\r\n  },\r\n  \"signature\": \"\",\r\n  \"data\": {\r\n";
-	
-	private static byte[] endStr =" ],\r\n    \"url\": \"\",\r\n    \"id\": 0\r\n  }\r\n}".getBytes();
-	
-	private String productName;
+
+    private static String partSuccStateStr = "{\r\n  \"response\": {\r\n    \"code\": 207,\r\n    \"message\": \"Part of the translation is available\",\r\n    \"serverTime\": \"\"\r\n  },\r\n  \"signature\": \"\",\r\n  \"data\": {\r\n";
+
+    private static String succStateStr = "{\r\n  \"response\": {\r\n    \"code\": 200,\r\n    \"message\": \"OK\",\r\n    \"serverTime\": \"\"\r\n  },\r\n  \"signature\": \"\",\r\n  \"data\": {\r\n";
+
+    private static byte[] endStr = " ],\r\n    \"url\": \"\",\r\n    \"id\": 0\r\n  }\r\n}".getBytes();
+
+    private String productName;
     private String version;
     private String dataOrigin = "";
     private String pseudo = "false";
     private boolean machineTranslation = false;
     private List<String> locales;
     private List<String> components;
-    
-	public StreamProductResp(String productName, String version, 
-			List<String> locales, List<String> components, String pseudo, boolean machineTranslation) {
-		this.productName = productName;
-		this.version = version;
-		if(dataOrigin != null) {
-		 this.pseudo = pseudo;
-		}
-		this.machineTranslation = machineTranslation;
-		this.locales = locales;
-		this.components = components;
-	}
-	
+
+    public StreamProductResp(String productName, String version,
+                             List<String> locales, List<String> components, String pseudo, boolean machineTranslation) {
+        this.productName = productName;
+        this.version = version;
+        if (dataOrigin != null) {
+            this.pseudo = pseudo;
+        }
+        this.machineTranslation = machineTranslation;
+        this.locales = locales;
+        this.components = components;
+    }
+
     public ByteBuffer getRespStartBytes(int respNumb) {
-			return ByteBuffer.wrap(getParamStr(respNumb).getBytes());
+        return ByteBuffer.wrap(getParamStr(respNumb).getBytes());
     }
-    
+
     public ByteBuffer getEndBytes() {
-			return ByteBuffer.wrap(endStr);
+        return ByteBuffer.wrap(endStr);
     }
-    
+
     private String getParamStr(int respNumb) {
-    	StringBuilder paramBuilder = new StringBuilder();
-    	
-		switch (respNumb) {
-		case 200:
-			paramBuilder.append(succStateStr);
-			break;
+        StringBuilder paramBuilder = new StringBuilder();
 
-		case 207:
-			paramBuilder.append(partSuccStateStr);
-			break;
+        switch (respNumb) {
+            case 200:
+                paramBuilder.append(succStateStr);
+                break;
 
-		default:
-			paramBuilder.append(fallbackStateStr);
-			break;
-		}
-  
-    	paramBuilder.append("    \"productName\": \""+this.productName+"\",\r\n" );
-    	paramBuilder.append("    \"version\": \""+this.version+"\",\r\n" );
-    	paramBuilder.append("    \"dataOrigin\": \""+this.dataOrigin+"\",\r\n" );
-    	paramBuilder.append("    \"pseudo\": "+this.pseudo+",\r\n" );
-    	paramBuilder.append("    \"machineTranslation\": "+String.valueOf(this.machineTranslation)+",\r\n" );
-    	getLocalesStr(paramBuilder);
-    	getComponentsStr(paramBuilder);
-    	paramBuilder.append("    \"bundles\": [\r\n");
+            case 207:
+                paramBuilder.append(partSuccStateStr);
+                break;
+
+            default:
+                paramBuilder.append(fallbackStateStr);
+                break;
+        }
+
+        paramBuilder.append("    \"productName\": \"" + this.productName + "\",\r\n");
+        paramBuilder.append("    \"version\": \"" + this.version + "\",\r\n");
+        paramBuilder.append("    \"dataOrigin\": \"" + this.dataOrigin + "\",\r\n");
+        paramBuilder.append("    \"pseudo\": " + this.pseudo + ",\r\n");
+        paramBuilder.append("    \"machineTranslation\": " + String.valueOf(this.machineTranslation) + ",\r\n");
+        getLocalesStr(paramBuilder);
+        getComponentsStr(paramBuilder);
+        paramBuilder.append("    \"bundles\": [\r\n");
         return paramBuilder.toString();
     }
-    
+
     private void getLocalesStr(StringBuilder paramBuilder) {
-    	paramBuilder.append(	"    \"locales\": [ " );
-    	int index=0;
-    	
-    	for(String localStr : this.locales) {
-          if(index != 0) {
-	         paramBuilder.append(",");
+        paramBuilder.append("    \"locales\": [ ");
+        int index = 0;
 
-    		}
-	        index++;
-    		paramBuilder.append("\"");
-    		paramBuilder.append(localStr);
-    		paramBuilder.append("\"");
-    		
-    		
-    	}
-    	paramBuilder.append(" ],\r\n");
-    	
+        for (String localStr : this.locales) {
+            if (index != 0) {
+                paramBuilder.append(",");
+
+            }
+            index++;
+            paramBuilder.append("\"");
+            paramBuilder.append(localStr);
+            paramBuilder.append("\"");
+
+
+        }
+        paramBuilder.append(" ],\r\n");
+
     }
-    
+
     private void getComponentsStr(StringBuilder paramBuilder) {
-    	paramBuilder.append("    \"components\": [ ");
-    	int index=0;
-    	for(String compStr: this.components) {
-    		if(index != 0) {
-   	         paramBuilder.append(",");
+        paramBuilder.append("    \"components\": [ ");
+        int index = 0;
+        for (String compStr : this.components) {
+            if (index != 0) {
+                paramBuilder.append(",");
 
-       		}
-   	        index++;
-    		paramBuilder.append("\"");
-    		paramBuilder.append(compStr);
-    		paramBuilder.append("\"");
-    	}
-    	paramBuilder.append(" ],\r\n");
+            }
+            index++;
+            paramBuilder.append("\"");
+            paramBuilder.append(compStr);
+            paramBuilder.append("\"");
+        }
+        paramBuilder.append(" ],\r\n");
     }
-	
 
 
 }
