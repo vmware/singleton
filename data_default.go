a12d6e28a7f243d0cbf322429d4a2dc7bbe40ba5
Xiaochao Li <48587632+Xiaochao8@users.noreply.github.com>
2023-02-14 10:59:57 +0000

    ccc

--- data_default.go
+++ data_default.go
@@ -5,38 +5,39 @@
 
 package sgtn
 
+// MapComponentMsgs is for a ComponentMsgs containing map
 type MapComponentMsgs struct {
 	messages          map[string]string
 	locale, component string
 }
 
-// Create a new Component
+// NewMapComponentMsgs is to create a new ComponentMsgs easily
 func NewMapComponentMsgs(messages map[string]string, locale, component string) *MapComponentMsgs {
 	return &MapComponentMsgs{messages: messages, locale: locale, component: component}
 }
 
-// Get a message by key
+// Get returns a message by key
 func (d *MapComponentMsgs) Get(key string) (value string, found bool) {
 	value, found = d.messages[key]
 	return
 }
 
-// Set a message by key
+// Set is to save a key-message pair
 func (d *MapComponentMsgs) Set(key, value string) {
 	d.messages[key] = value
 }
 
-// Return number of the messages
+// Size returns the number of messages
 func (d *MapComponentMsgs) Size() int {
 	return len(d.messages)
 }
 
-// Return locale of the component
+// Locale returns the locale of component
 func (d *MapComponentMsgs) Locale() string {
 	return d.locale
 }
 
-// Return the component name
+// Component returns the component name
 func (d *MapComponentMsgs) Component() string {
 	return d.component
 }
