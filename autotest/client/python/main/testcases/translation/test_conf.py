e4864396b4d99487448fdc8b9c4d39f2ea07a44f
mabeijing <58149278@qq.com>
2023-02-28 17:48:23 +0800

    add bundle

--- autotest/client/python/main/testcases/translation/test_conf.py
+++ autotest/client/python/main/testcases/translation/test_conf.py
@@ -10,7 +10,7 @@ VERSION = '1.0.0'
 COMPONENT = 'about'
 LOCALE = 'fr'
 CONFIG_FILE = 'config_only.yml'
-__RESOURCES__ = Path(__file__).parent.joinpath('config')
+_RESOURCES_ = Path(__file__).parent.joinpath('config')
 
 
 class TestConfig:
@@ -18,7 +18,7 @@ class TestConfig:
     def test_l1(self):
         """config success"""
 
-        file: Path = __RESOURCES__.joinpath(CONFIG_FILE)
+        file: Path = _RESOURCES_.joinpath(CONFIG_FILE)
         I18N.add_config_file(file)
         rel = I18N.get_release(PRODUCT, VERSION)
         conf = rel.get_config()
@@ -30,14 +30,14 @@ class TestConfig:
         extra_config: dict = {'product': 'PythonClient', 'l10n_version': '2.0.0',
                               'online_service_url': 'http://localhost:8091', 'try_delay': 10,
                               'cache_expired_time': 600}
-        file: Path = __RESOURCES__.joinpath('only_online.yml')
+        file: Path = _RESOURCES_.joinpath('only_online.yml')
         slc = I18N.add_config(file, extra_config)
         con = slc.get_config_data()
         assert con == extra_config
 
     def test_l3(self):
         """set_locale"""
-        file: Path = __RESOURCES__.joinpath(CONFIG_FILE)
+        file: Path = _RESOURCES_.joinpath(CONFIG_FILE)
         I18N.add_config_file(file)
 
         I18N.set_current_locale(LOCALE)
