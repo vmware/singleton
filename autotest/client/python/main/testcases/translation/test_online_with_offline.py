835f35713a35b9003ed55fe6e008ae5bf954ceec
mabeijing <58149278@qq.com>
2023-03-09 13:18:16 +0800

    update mixed

--- autotest/client/python/main/testcases/translation/test_online_with_offline.py
+++ autotest/client/python/main/testcases/translation/test_online_with_offline.py
@@ -1,6 +1,3 @@
-import time
-from multiprocessing import Process
-
 import pytest
 from pathlib import Path
 from sgtnclient import I18N
@@ -12,10 +9,7 @@ COMPONENT = 'about'
 LOCALE = 'fr'
 Config_files = 'sgtn_online_offline_before.yml'
 
-# singleton\test\TRANSLATION
-__TRANSLATION__ = Path(__file__).parent
-__CONFIG__ = __TRANSLATION__.joinpath('config')
-__RESOURCES__ = __TRANSLATION__.joinpath('resources')
+__CONFIG__ = Path(__file__).parent.joinpath('config')
 
 
 class TestOnlineWithOffline:
@@ -26,12 +20,12 @@ class TestOnlineWithOffline:
         file: Path = __CONFIG__.joinpath('sgtn_online_offline_before.yml')
         I18N.add_config_file(file)
         I18N.set_current_locale(LOCALE)
-        current = I18N.get_current_locale()
-        print(current)
-        self.rel = I18N.get_release(PRODUCT, VERSION)
-        translation = self.rel.get_translation()
+        rel = I18N.get_release(PRODUCT, VERSION)
+        translation = rel.get_translation()
+
         # tran1 = translation.get_string("intest","intest.offline")
         # assert tran1 == "Contact1 offline"
+
         tran2 = translation.get_string("intest", "intest.offline", locale="ja")
         assert tran2 == "offline12345"
 
@@ -47,10 +41,10 @@ class TestOnlineWithOffline:
             tran1 = translation.get_string("about", "about.message")
             assert tran1 == "test__de_value"
 
-        time.sleep(5)
+        # time.sleep(5)
         tran2 = translation.get_string("about", "about.message")
         assert tran2 == "test__de_value"
-        time.sleep(1)
+        # time.sleep(1)
         tran2 = translation.get_string("about", "about.message")
         assert tran2 == "test__de_value"
 
@@ -59,12 +53,16 @@ class TestOnlineWithOffline:
         print("the key only in online")
         file: Path = __CONFIG__.joinpath('sgtn_online_offline_before.yml')
         I18N.add_config_file(file)
-        I18N.set_current_locale("fr")
-        self.rel = I18N.get_release(PRODUCT, VERSION)
-        translation = self.rel.get_translation()
-        # tran1 = translation.get_string("intest","intest.online",source = "Contact1 online")
-        tran1 = translation.get_string("intest", "intest.online")
+        rel = I18N.get_release(PRODUCT, VERSION)
+        translation = rel.get_translation()
+
+        # only online key 不比较
+        tran1 = translation.get_string("intest", "intest.online", locale='fr')
         assert tran1 == "Contact1 fr online"
+
+        # 仅本地key，直接返回source
+        tran1 = translation.get_string("intest", "intest.offline", locale='de')
+        assert tran1 == "offline12345"
 
     @pytest.mark.ci1
     def test_l3(self):
@@ -72,10 +70,9 @@ class TestOnlineWithOffline:
         I18N.add_config_file(file)
         I18N.set_current_locale(LOCALE)
 
-        self.rel = I18N.get_release(PRODUCT, VERSION)
-        translation1 = self.rel.get_translation()
-        tran2 = translation1.get_string("about", "about.message", locale='fr-CA')
-        print(tran2)
+        rel = I18N.get_release(PRODUCT, VERSION)
+        translation = rel.get_translation()
+        tran2 = translation.get_string("about", "about.message", locale='fr-CA')
         assert tran2 == "test fr key"
 
     @pytest.mark.citest
@@ -83,22 +80,22 @@ class TestOnlineWithOffline:
         file: Path = __CONFIG__.joinpath('sgtn_online_offline_before.yml')
         I18N.add_config_file(file)
         I18N.set_current_locale(LOCALE)
-        self.rel = I18N.get_release(PRODUCT, VERSION)
-        translation1 = self.rel.get_translation()
-        trans2 = translation1.get_string("intest", "intest.home", locale="fr")
+        rel = I18N.get_release(PRODUCT, VERSION)
+        translation = rel.get_translation()
+        trans2 = translation.get_string("intest", "intest.home", locale="fr")
         assert trans2 == "Home12"
 
     def test_l5(self):
         file: Path = __CONFIG__.joinpath('sgtn_online_offline_before.yml')
         I18N.add_config_file(file)
         I18N.set_current_locale(LOCALE)
-        self.rel = I18N.get_release(PRODUCT, VERSION)
-        translation1 = self.rel.get_translation()
-        trans2 = translation1.get_string("intest", "intest.about", locale="ja")
+        rel = I18N.get_release(PRODUCT, VERSION)
+        translation = rel.get_translation()
+        trans2 = translation.get_string("intest", "intest.about", locale="ja")
         assert trans2 == "About1 ja"
-        trans3 = translation1.get_string("intest", "intest.about", source="About1")
+        trans3 = translation.get_string("intest", "intest.about", source="About1")
         assert trans3 == "About1 fr"
-        trans4 = translation1.get_string("intest", "intest.about", source="About1", locale="ja")
+        trans4 = translation.get_string("intest", "intest.about", source="About1", locale="ja")
         assert trans4 == "About1 ja"
 
     @pytest.mark.ci1
@@ -110,9 +107,9 @@ class TestOnlineWithOffline:
         I18N.add_config_file(file)
         I18N.set_current_locale(LOCALE)
         rel = I18N.get_release(PRODUCT, VERSION)
-        translation1 = rel.get_translation()
-        trans1 = translation1.get_string("common", "common.about")
-        assert trans1 == "about1"
+        translation = rel.get_translation()
+        trans1 = translation.get_string("common", "common.about")
+        assert trans1 == "test fr key contact online with localsource"
 
     @pytest.mark.skip
     def test_l7(self):
@@ -120,32 +117,32 @@ class TestOnlineWithOffline:
         I18N.add_config_file(file)
         I18N.set_current_locale(LOCALE)
         rel = I18N.get_release(PRODUCT, VERSION)
-        translation1 = rel.get_translation()
-        trans1 = translation1.get_string("index", "index.title")
+        translation = rel.get_translation()
+        trans1 = translation.get_string("index", "index.title")
         assert trans1 == "Sample fr Application"
 
-    @pytest.mark.ci1
-    def test_l8(self):
-        file: Path = __CONFIG__.joinpath('sgtn_online_offline_before.yml')
-        I18N.add_config_file(file)
-        I18N.set_current_locale(LOCALE)
-        self.rel = I18N.get_release(PRODUCT, VERSION)
-        translation1 = self.rel.get_translation()
-        trans1 = translation1.get_string("contact", "contact.support", source="Support:", locale="ko")
-        assert trans1 == "サポート：ko"
-        trans1 = translation1.get_string("contact", "contact.support", locale="ko")
-        assert trans1 == "サポート：ko"
+    # @pytest.mark.ci1
+    # def test_l8(self):
+    #     file: Path = __CONFIG__.joinpath('sgtn_online_offline_before.yml')
+    #     I18N.add_config_file(file)
+    #     I18N.set_current_locale(LOCALE)
+    #     rel = I18N.get_release(PRODUCT, VERSION)
+    #     translation = rel.get_translation()
+    #     trans1 = translation.get_string("contact", "contact.support", source="Support:", locale="ko")
+    #     assert trans1 == "サポート：ko"
+    #     trans1 = translation.get_string("contact", "contact.support", locale="ko")
+    #     assert trans1 == "サポート：ko"
 
     @pytest.mark.ci1
     def test_l9(self):
         file: Path = __CONFIG__.joinpath('sgtn_online_offline_before.yml')
         I18N.add_config_file(file)
         I18N.set_current_locale(LOCALE)
-        self.rel = I18N.get_release(PRODUCT, VERSION)
-        translation1 = self.rel.get_translation()
-        trans1 = translation1.get_string("contact", "contact.support", source="Support:", locale="ja")
+        rel = I18N.get_release(PRODUCT, VERSION)
+        translation = rel.get_translation()
+        trans1 = translation.get_string("contact", "contact.support", source="Support:", locale="ja")
         assert trans1 == "サポート："
-        trans2 = translation1.get_string("contact", "contact.support", locale="ja")
+        trans2 = translation.get_string("contact", "contact.support", locale="ja")
         assert trans2 == "サポート："
 
     @pytest.mark.ci1
@@ -153,14 +150,13 @@ class TestOnlineWithOffline:
         file: Path = __CONFIG__.joinpath('sgtn_online_offline_before.yml')
         I18N.add_config_file(file)
         I18N.set_current_locale(LOCALE)
-        self.rel = I18N.get_release(PRODUCT, VERSION)
-        translation1 = self.rel.get_translation()
-        trans1 = translation1.get_string("contact", "contact.title", locale="pt")
+        rel = I18N.get_release(PRODUCT, VERSION)
+        translation = rel.get_translation()
+        trans1 = translation.get_string("contact", "contact.title", locale="pt")
         # source= "Contact",
         assert trans1 == "Contact"
 
-    @staticmethod
-    def mix_get_locale_strings():
+    def test_mix_get_locale_strings(self):
         file: Path = __CONFIG__.joinpath('sgtn_online_offline_before.yml')
         I18N.add_config_file(file)
         I18N.set_current_locale(LOCALE)
@@ -191,13 +187,6 @@ class TestOnlineWithOffline:
         assert "about" not in trans1.keys()
 
     @pytest.mark.ci1
-    def test_mix_get_locale_strings(self):
-        task = Process(target=self.mix_get_locale_strings)
-        task.daemon = True
-        task.start()
-        task.join()
-
-    @pytest.mark.ci1
     def test_mix_format_items(self):
         """
         mix mode: format items
@@ -205,18 +194,18 @@ class TestOnlineWithOffline:
         file: Path = __CONFIG__.joinpath('sgtn_online_offline_before.yml')
         I18N.add_config_file(file)
         I18N.set_current_locale(LOCALE)
-        self.rel = I18N.get_release(PRODUCT, VERSION)
-        translation1 = self.rel.get_translation()
-        trans1 = translation1.get_string("about", "about.test", x="1", y="2")
+        rel = I18N.get_release(PRODUCT, VERSION)
+        translation = rel.get_translation()
+        trans1 = translation.get_string("about", "about.test", x="1", y="2")
         assert trans1 == "test fr the {1} to {0} and {2}"
-        trans1 = translation1.get_string("about", "about.test", format_items=["11", "22", "33"])
+        trans1 = translation.get_string("about", "about.test", format_items=["11", "22", "33"])
         assert trans1 == "test fr the 22 to 11 and 33"
-        trans1 = translation1.get_string("about", "about.test", format_items=["11", "22", "33", "44"])
+        trans1 = translation.get_string("about", "about.test", format_items=["11", "22", "33", "44"])
         assert trans1 == "test fr the 22 to 11 and 33"
-        trans2 = translation1.get_string("contact", "contact.title", format_items={'x': '11', 'y': '22', 'z': '33'},
-                                         locale='ko')
-        assert trans2 == "연락처change 22 add 11 and 33"
-        trans1 = translation1.get_string("about", "about.test", format_items=["11", None, "33"])
+        # trans2 = translation.get_string("contact", "contact.title", format_items={'x': '11', 'y': '22', 'z': '33'},
+        #                                 locale='ko')
+        # assert trans2 == "연락처change 22 add 11 and 33"
+        trans1 = translation.get_string("about", "about.test", format_items=["11", None, "33"])
         assert trans1 == "test fr the None to 11 and 33"
 
     @pytest.mark.ci1
@@ -224,11 +213,11 @@ class TestOnlineWithOffline:
         file: Path = __CONFIG__.joinpath('sgtn_online_offline_before.yml')
         I18N.add_config_file(file)
         I18N.set_current_locale(LOCALE)
-        self.rel = I18N.get_release(PRODUCT, VERSION)
-        translation1 = self.rel.get_translation()
-        trans1 = translation1.get_string("about", "about.message")
+        rel = I18N.get_release(PRODUCT, VERSION)
+        translation = rel.get_translation()
+        trans1 = translation.get_string("about", "about.message")
         print(trans1)
-        trans2 = translation1.get_string("contact", "about.message")
+        trans2 = translation.get_string("contact", "about.message")
         print(trans2)
 
 
