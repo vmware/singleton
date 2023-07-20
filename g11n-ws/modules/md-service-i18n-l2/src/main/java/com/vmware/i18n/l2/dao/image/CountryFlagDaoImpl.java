8a53d9e99c58b2131928b498b46ad4f7cf9cc195
Shi Hu <shihu@vmware.com>
2023-07-20 17:59:33 +0800

    put source file in zip file

--- g11n-ws/modules/md-service-i18n-l2/src/main/java/com/vmware/i18n/l2/dao/image/CountryFlagDaoImpl.java
+++ g11n-ws/modules/md-service-i18n-l2/src/main/java/com/vmware/i18n/l2/dao/image/CountryFlagDaoImpl.java
@@ -22,111 +22,99 @@ import org.springframework.util.ResourceUtils;
 import java.io.*;
 import java.nio.ByteBuffer;
 import java.nio.channels.FileChannel;
+import java.nio.charset.Charset;
 import java.nio.charset.StandardCharsets;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipInputStream;
 
 @Repository
 public class CountryFlagDaoImpl implements ICountryFlagDao {
     private static Logger logger = LoggerFactory.getLogger(CountryFlagDaoImpl.class);
-    private static String basePath = ConstantsKeys.IMAGE+File.separator+ConstantsKeys.FLAGS+File.separator;
+    private static String basePath = ConstantsKeys.IMAGE + File.separator + ConstantsKeys.FLAGS + File.separator;
 
-    protected void initZipCountryFlagPattern()  {
+    @PostConstruct
+    protected void initZipCountryFlagPattern() {
 
         logger.info("begin to init country flag content.");
-        String sourcePath = "flag/country-flag-icons**.zip";
-        Resource resource   = new PathMatchingResourcePatternResolver().getResource(ResourceUtils.CLASSPATH_URL_PREFIX+sourcePath);
-        harset gbk = Charset.forName("gbk");
-        ZipInputStream zin = new ZipInputStream(in,gbk);
-        ZipEntry ze;
-        while((ze = zin.getNextEntry()) != null){
-            if(ze.toString().endsWith("txt")){
-                BufferedReader br = new BufferedReader(
-                        new InputStreamReader(zf.getInputStream(ze)));
-                String line;
-                while((line = br.readLine()) != null){
-                    System.out.println(line.toString());
-                }
-                br.close();
+        String sourcePath = "flag/country-flag-icons-**.zip";
+        try {
+            Resource[] resources = resources = new PathMatchingResourcePatternResolver()
+                    .getResources(ResourceUtils.CLASSPATH_URL_PREFIX + sourcePath);
+            if (resources.length == 1) {
+               try (ZipInputStream zin = new ZipInputStream(resources[0].getInputStream(), Charset.forName("UTF-8"))) {
+                   ZipEntry ze = null;
+                   while ((ze = zin.getNextEntry()) != null) {
+                       String zipName = ze.getName().substring(ze.getName().indexOf(ConstantsChar.BACKSLASH));
+                       if ((zipName.startsWith("/flags/3x2/") || zipName.startsWith("/flags/1x1/")) && zipName.endsWith(ConstantsFile.FILE_TYPE_SVG)) {
+                           try (ByteArrayOutputStream bos = new ByteArrayOutputStream()) {
+                               byte[] buffer = new byte[1024];
+                               int len;
+                               while ((len = zin.read(buffer)) > 0) {
+                                   bos.write(buffer, 0, len);
+                               }
+                               String content = bos.toString();
+                               writeCountryFlagResult(zipName, content);
+                           }
+                       }
+                       zin.closeEntry();
+                   }
+               }
+                logger.info("Init zip country flag content successfully!");
+
+            } else {
+                logger.error("only allows one country flag source zip file");
             }
-            System.out.println();
+        } catch (IOException e) {
+            logger.error("Init zip country flag content failure!", e);
         }
-        zin.closeEntry();
-
-
-
-
-        logger.info("Init country flag content successfully!");
-
 
     }
 
-        @PostConstruct
-    protected void initCountryFlagPattern()  {
-        logger.info("begin to init country flag content.");
-        String sourcePath = "flags/**/**.svg";
-        try {
-            Resource[] resources  = resources = new PathMatchingResourcePatternResolver()
-                .getResources(ResourceUtils.CLASSPATH_URL_PREFIX+sourcePath);
+    private void writeCountryFlagResult(String sourcePathStr, String flagContent) throws IOException {
 
-            for(Resource resource: resources) {
-                String pathStr = resource.getURL().getFile();
+        String pathStr = ConstantsKeys.IMAGE + sourcePathStr;
+        logger.info(pathStr);
+        pathStr = pathStr.replaceAll(ConstantsChar.BACKSLASH, File.separator);
+        pathStr = pathStr.replaceAll(ConstantsFile.FILE_TYPE_SVG, ConstantsFile.FILE_TPYE_JSON);
+        File file = new File(pathStr);
+        if (!file.getParentFile().exists()) {
+            file.getParentFile().mkdirs();
+        }
+        file.deleteOnExit();
+        file.createNewFile();
+        String region = file.getName().replace(ConstantsFile.FILE_TPYE_JSON, "");
 
-                int index = pathStr.lastIndexOf("!/flags/")+1;
-                pathStr = ConstantsKeys.IMAGE+pathStr.substring(index);
-                logger.info(pathStr);
-                pathStr = pathStr.replaceAll(ConstantsChar.BACKSLASH, File.separator);
-                pathStr = pathStr.replaceAll(ConstantsFile.FILE_TYPE_SVG, ConstantsFile.FILE_TPYE_JSON);
-                File file = new File(pathStr);
-                if (!file.getParentFile().exists()){
-                    file.getParentFile().mkdirs();
-                }
-                file.deleteOnExit();
-                file.createNewFile();
-                String region = file.getName().replace(ConstantsFile.FILE_TPYE_JSON,"");
+        Map<String, String> respData = new HashMap<>();
+        respData.put("type", "svg");
+        respData.put("image", flagContent);
+        respData.put("region", region);
+        APIResponseDTO apiResponseDTO = new APIResponseDTO();
+        apiResponseDTO.setData(respData);
+        apiResponseDTO.setResponse(APIResponseStatus.OK);
 
-                StringBuilder sb = new StringBuilder();
-                try(BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(resource.getInputStream()))){
-                    String temp = null;
-                    while((temp = bufferedReader.readLine()) != null){
-                        sb.append(temp).append("\n");
-                    }
-                }
-                Map<String,String> respData = new HashMap<>();
-                respData.put("type", "svg");
-                respData.put("image", sb.toString());
-                respData.put("region", region);
-                APIResponseDTO apiResponseDTO = new APIResponseDTO();
-                apiResponseDTO.setData(respData);
-                apiResponseDTO.setResponse(APIResponseStatus.OK);
-
-                try (FileOutputStream fileOutputStream = new FileOutputStream(file);
-                     FileChannel fileChannel = fileOutputStream.getChannel() ) {
-                    String content = null;
-                    try {
-                        content = new ObjectMapper().writerWithDefaultPrettyPrinter().writeValueAsString(apiResponseDTO);
-                    } catch (JsonProcessingException e) {
-                        logger.error(e.getMessage(), e);
-                        throw e;
-                    }
-                    fileChannel.write(ByteBuffer.wrap(content.getBytes(StandardCharsets.UTF_8)));
-
-                } catch (IOException e) {
-                    logger.error(e.getMessage(), e);
-                    throw e;
-                }
-
+        try (FileOutputStream fileOutputStream = new FileOutputStream(file);
+             FileChannel fileChannel = fileOutputStream.getChannel()) {
+            String content = null;
+            try {
+                content = new ObjectMapper().writerWithDefaultPrettyPrinter().writeValueAsString(apiResponseDTO);
+            } catch (JsonProcessingException e) {
+                logger.error(e.getMessage(), e);
+                throw e;
             }
+            fileChannel.write(ByteBuffer.wrap(content.getBytes(StandardCharsets.UTF_8)));
 
         } catch (IOException e) {
-            logger.error("Init the country flag content failure!", e);
+            logger.error(e.getMessage(), e);
+            throw e;
         }
-        logger.info("Init country flag content successfully!");
 
     }
 
+
     @Override
-    public FileChannel getCountryFlagChannel(String scale, String shortName) throws Exception{
+    public FileChannel getCountryFlagChannel(String scale, String shortName) throws Exception {
 
         StringBuilder sourcePath = new StringBuilder(basePath);
         sourcePath.append(scale.replaceAll("\\.", "").replaceAll("/", "")).append(File.separator);
