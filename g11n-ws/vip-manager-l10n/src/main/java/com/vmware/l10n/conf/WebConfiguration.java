73d0575f7acf419d47f6fb5e748eb109e74b28fc
Shi Hu <shihu@vmware.com>
2023-07-27 11:47:17 +0800

    fixed specal

--- g11n-ws/vip-manager-l10n/src/main/java/com/vmware/l10n/conf/WebConfiguration.java
+++ g11n-ws/vip-manager-l10n/src/main/java/com/vmware/l10n/conf/WebConfiguration.java
@@ -4,10 +4,14 @@
  */
 package com.vmware.l10n.conf;
 
+import org.apache.tomcat.util.buf.EncodedSolidusHandling;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
+import org.springframework.boot.web.server.WebServerFactoryCustomizer;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
@@ -68,6 +72,12 @@ public class WebConfiguration implements WebMvcConfigurer {
 	@Override
 	public void addCorsMappings(CorsRegistry registry) {
 		registry.addMapping("/**").allowedOrigins("*");
+	}
+
+
+     @Bean
+	public WebServerFactoryCustomizer<TomcatServletWebServerFactory> tomcatCustomizer() {
+		return factory -> factory.addConnectorCustomizers(connector -> {connector.setEncodedSolidusHandling(EncodedSolidusHandling.DECODE.getValue()); connector.setAllowBackslash(true);});
 	}
 
 }
