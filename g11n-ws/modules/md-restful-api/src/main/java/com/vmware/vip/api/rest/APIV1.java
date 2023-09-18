ff75b908a0252015ece8b48cfb98b3f404c3ee37
Shi Hu <shihu@vmware.com>
2023-09-18 17:08:00 +0800

    fixed the performance

--- g11n-ws/modules/md-restful-api/src/main/java/com/vmware/vip/api/rest/APIV1.java
+++ g11n-ws/modules/md-restful-api/src/main/java/com/vmware/vip/api/rest/APIV1.java
@@ -21,7 +21,7 @@ public class APIV1 {
     public static final String TRANS_STRING            = API.I18N_API_ROOT + V + "/translation/string";
     public static final String TRANSLATION             = API.I18N_API_ROOT + V + "/translation";
     public static final String SUPPORTED_LOCALES       = API.I18N_API_ROOT + V + "/translation/product/{" + APIParamName.PRODUCT_NAME + "}/version/{" + APIParamName.VERSION + "}/supportedLocales";
-    public static final String COMPONENTS              = API.L10N_API_ROOT + V + "/bundles/components";
+    public static final String COMPONENTS              = API.I18N_API_ROOT + V + "/bundles/components";
     public static final String COMPONENTS2             = API.I18N_API_ROOT + V + "/translation/product/{" + APIParamName.PRODUCT_NAME + "}/components/{" + APIParamName.COMPONENTS + "}";
     public static final String COMPONENT               = API.I18N_API_ROOT + V + "/translation/product/{" + APIParamName.PRODUCT_NAME + "}/component/{" + APIParamName.COMPONENT + "}";
     public static final String KEY2_GET                = API.I18N_API_ROOT + V + "/translation/product/{" + APIParamName.PRODUCT_NAME + "}/component/{" + APIParamName.COMPONENT + "}/key/{" + APIParamName.KEY2 + "}";
