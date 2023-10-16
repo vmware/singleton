bc03aac7ec71dfe217f500893f529a71da3f227d
Shi Hu <shihu@vmware.com>
2023-10-16 14:14:56 +0800

    update unit test

--- g11n-ws/vip-manager-l10n/src/test/java/com/vmware/controller/SourceDaoTest.java
+++ g11n-ws/vip-manager-l10n/src/test/java/com/vmware/controller/SourceDaoTest.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2019-2022 VMware, Inc.
+ * Copyright 2019-2023 VMware, Inc.
  * SPDX-License-Identifier: EPL-2.0
  */
 package com.vmware.controller;
@@ -16,7 +16,6 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.web.context.WebApplicationContext;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
 import com.vmware.l10n.BootApplication;
 import com.vmware.l10n.source.dao.SourceDao;
 import com.vmware.vip.common.l10n.source.dto.ComponentMessagesDTO;
@@ -32,7 +31,7 @@ public class SourceDaoTest {
 
 	
 @Test
-public void  test001() throws JsonProcessingException {
+public void  test001() throws Exception {
 	SourceDao sourceDao = webApplicationContext.getBean(SourceDao.class);	
 	ComponentMessagesDTO single= new ComponentMessagesDTO();
 	
